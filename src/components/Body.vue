b583488f85a6316f1e643d142a543e6091f247fb
gunwoongPark <bung1438@gmail.com>
2020-07-14 21:40:58 +0900

    Body.vue 컴포넌트 이미지 경로 추가, store디렉토리의 state에 공지사항의 이미지 경로 데이터 추가
    공지사항 게시글의 본문에 마크다운 언어 외에 이미지 1장이 등록될 수 있으므로 이미지를 삽입할 수 있는 데이터를 추가하였다.

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container>
     <div>{{notice_body.md_text}}</div>
+    <img :src="notice_body.image_path" alt="예지쨩 사진" width="300px" />
   </v-container>
 </template>
 
--- src/store/index.js
+++ src/store/index.js
@@ -12,7 +12,8 @@ export default new Vuex.Store({
       views: "999"
     },
     notice_body: {
-      md_text: "마크다운으로 작성된 글"
+      md_text: "마크다운으로 작성된 글",
+      image_path: "https://upload.wikimedia.org/wikipedia/commons/b/bb/Hwang_Ye-ji_going_to_a_Music_Bank_recording_on_August_8%2C_2019.png"
     }
   },
   mutations: {
