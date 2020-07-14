76af6fdcc84d49e736bf45a6eecc8627b37ab44c
gunwoongPark <bung1438@gmail.com>
2020-07-14 21:11:52 +0900

    버튼을 누르면 즉시 페이지 상단으로 스크롤이 이동하는 버튼 컴포넌트 구현 - TopBtn.vue
    views 디렉토리에 Test.vue 라는 페이지에서 테스트할 수 있다.
    Test.vue는 개행문자가 매우 많아 스크롤이 가능한 페이지이다.
    버튼을 누르면 스크롤의 길이에 상관 없이 일정한 속도로 부드럽게 상단으로 이동한다.

---
+++ src/components/TopBtn.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-btn color="primary" class="top_btn" @click="go_top">TopButton</v-btn>
+</template>
+
+<script>
+export default {
+  methods: {
+    go_top() {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.top_btn {
+  position: fixed;
+  bottom: 15px;
+  right: 15px;
+}
+</style>
