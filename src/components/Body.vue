4f54cac75d511a1196f3f93a3b3e0e58053250a6
gunwoongPark <bung1438@gmail.com>
2020-07-15 14:27:42 +0900

    마크다운 기능 구현
    npm i vue-markdown을 터미널에 작성하여 마크다운 에디터 사용

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,12 +1,16 @@
 <template>
   <v-container>
-    <div>{{notice_body.md_text}}</div>
+    <VueMarkDown>{{notice_body.md_text}}</VueMarkDown>
     <img :src="notice_body.image_path" alt="예지쨩 사진" width="300px" />
   </v-container>
 </template>
 
 <script>
+import VueMarkDown from "vue-markdown";
 export default {
+  components: {
+    VueMarkDown
+  },
   computed: {
     notice_body() {
       return this.$store.state.notice_body;
