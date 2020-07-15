70a4889d42152f72d93cdf187dc14fda53444ac6
suhyeondev <contact.suhyeon@gmail.com>
2020-07-15 21:31:05 +0900

    2020.7.15 레이아웃을 그리드로 변경 및 디자인 수정

--- src/views/ProjectWrite.vue
+++ src/views/ProjectWrite.vue
@@ -5,44 +5,61 @@
         <v-container>
           <v-card>
             <v-alert outlined color="#226db2">
-              <v-card-title>공고 쓰기</v-card-title>
-              <v-form v-model="isValid" ref="myform">
-                <v-text-field label="모집인원" required></v-text-field>
-                <v-text-field label="제목" required></v-text-field>
-                <v-text-field label="내용" required></v-text-field>
-                <v-flex>
-                  <v-menu
-                    ref="menu"
-                    v-model="menu"
-                    :close-on-content-click="false"
-                    :return-value.sync="date"
-                    lazy
-                    transition="scale-transition"
-                    offset-y
-                    full-width
-                    min-width="290px"
-                  >
-                    <template v-slot:activator="{ on }">
-                      <v-text-field v-model="date" label="마감일" readonly v-on="on"></v-text-field>
-                    </template>
-                    <v-date-picker v-model="date" no-title scrollable>
-                      <v-spacer></v-spacer>
-                      <v-btn flat color="primary" @click="menu = false">취소</v-btn>
-                      <v-btn flat color="primary" @click="$refs.menu.save(date)">확인</v-btn>
-                    </v-date-picker>
-                  </v-menu>
-                </v-flex>
-                <v-file-input label="파일 첨부" outlined v-model="chosenFile"></v-file-input>
+              <v-form v-model="isValid" ref="myform
+              ">
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field label="제목" required></v-text-field>
+                  </v-col>
+                </v-row>
 
-                <v-layout>
-                  <v-flex>
+                <v-row>
+                  <v-col cols="6">
+                    <v-text-field label="모집인원" required></v-text-field>
+                  </v-col>
+                  <v-col cols="6">
+                    <v-menu
+                      ref="menu"
+                      v-model="menu"
+                      :close-on-content-click="false"
+                      :return-value.sync="date"
+                      lazy
+                      transition="scale-transition"
+                      offset-y
+                      full-width
+                      min-width="290px"
+                    >
+                      <template v-slot:activator="{ on }">
+                        <v-text-field v-model="date" label="마감일" readonly v-on="on"></v-text-field>
+                      </template>
+                      <v-date-picker v-model="date" no-title scrollable>
+                        <v-spacer></v-spacer>
+                        <v-btn flat color="primary" @click="menu = false">취소</v-btn>
+                        <v-btn flat color="primary" @click="$refs.menu.save(date)">확인</v-btn>
+                      </v-date-picker>
+                    </v-menu>
+                  </v-col>
+                </v-row>
+
+                <v-row>
+                  <v-col cols="12">
+                    <v-file-input label="파일 첨부" v-model="chosenFile"></v-file-input>
+                  </v-col>
+                </v-row>
+
+                <v-row>
+                  <v-col cols="12">
                     <vue-markdown>
-                      <v-textarea label="여기에 내용을 작성해 주세요! (마크다운)"></v-textarea>
+                      <v-textarea outlined label="내용 (마크다운)"></v-textarea>
                     </vue-markdown>
-                  </v-flex>
-                </v-layout>
+                  </v-col>
+                </v-row>
 
-                <v-btn color="#226db2">완료</v-btn>
+                <v-row>
+                  <v-col cols="12">
+                    <v-btn color="primary">완료</v-btn>
+                  </v-col>
+                </v-row>
               </v-form>
             </v-alert>
           </v-card>
@@ -59,4 +76,7 @@ export default {
 </script>
 
 <style>
+.v-input__prepend-outer {
+  display: none !important;
+}
 </style>
