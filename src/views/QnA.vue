0013be6769196227bb78660dc90407c7a5a39e6c
hayoungzzzzz <jhy901@naver.com>
2020-07-17 19:00:28 +0900

    기본 틀 구현

---
+++ src/views/QnA.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-app>
+        <v-content>
+            <v-container>
+            
+            </v-container>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
