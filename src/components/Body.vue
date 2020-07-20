df482b6b1a3db57724df615ffedae791a360bee5
gunwoongPark <bung1438@gmail.com>
2020-07-20 18:57:42 +0900

    상단의 최초 질문 추가 및 데이터 구조 변경

---
+++ src/components/Body.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-col>
+    <img src="BodyInfo.Image" alt="예지쨩 사진" />
+    <vue-markdown>{{BodyInfo.Contents}}</vue-markdown>
+  </v-col>
+</template>
+
+<script>
+import VueMarkdown from "vue-markdown"; //markdown import
+export default {
+  components: {
+    VueMarkdown
+  },
+  props: {
+    BodyInfo: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
--- src/views/QnA.vue
+++ src/views/QnA.vue
@@ -1,5 +1,7 @@
 <template>
   <v-container>
+    <Problem :problemInfo="problemInfo" />
+    <hr />
     <CommentList :commentData="commentData" />
     <hr />
     <br />
@@ -10,16 +12,17 @@
 <script>
 import CommentList from "../components/CommentList";
 import CommentForm from "../components/CommentForm";
+import Problem from "../components/Problem.vue";
 export default {
   components: {
     CommentList,
-    CommentForm
+    CommentForm,
+    Problem
   },
   data() {
     return {
       commentData: {
         writerName: "18 전하영",
-
         comments: [
           { name: "16 박건웅", comment: "답변 1" },
           { name: "17 김아무개", comment: "답변 2" },
@@ -27,6 +30,20 @@ export default {
           { name: "19 이아무개", comment: "답변 3" },
           { name: "20 정아무개", comment: "답변 4" }
         ]
+      },
+
+      problemInfo: {
+        Header: {
+          Title: "제목",
+          writerName: "18 전하영",
+          writeDate: "2020년 07월 20일",
+          countViews: 999
+        },
+        Body: {
+          Contents: "# 마크다운으로 작성된 글",
+          Image:
+            "https://i.pinimg.com/originals/d7/8f/a2/d78fa2deed379f6668e2995ac38362a2.jpg"
+        }
       }
     };
   }
