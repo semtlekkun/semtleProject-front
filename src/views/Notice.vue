6ae711d6504ee31b797efdbe516a53d4ee278abc
gunwoongPark <bung1438@gmail.com>
2020-07-26 01:26:28 +0900

    내용 콘텐츠 예제 긴 문장으로 변경

--- src/views/Notice.vue
+++ src/views/Notice.vue
@@ -13,7 +13,7 @@ import Body from "../components/Body.vue";
 export default {
   components: {
     Header,
-    Body
+    Body,
   },
   data() {
     return {
@@ -21,14 +21,14 @@ export default {
         title: "제목제목제목제목제목제목",
         writer: "홍길동",
         date: "2020년 7월 14일",
-        views: "999"
+        views: "999",
       },
       noticeBody: {
-        mdText: `## 마크다운으로 작성된 글`,
+        mdText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
         imagePath:
-          "https://upload.wikimedia.org/wikipedia/commons/b/bb/Hwang_Ye-ji_going_to_a_Music_Bank_recording_on_August_8%2C_2019.png"
-      }
+          "https://upload.wikimedia.org/wikipedia/commons/b/bb/Hwang_Ye-ji_going_to_a_Music_Bank_recording_on_August_8%2C_2019.png",
+      },
     };
-  }
+  },
 };
 </script>
