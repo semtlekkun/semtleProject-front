3e412771599c073dff48238779b7074f927f58ed
gunwoongPark <bung1438@gmail.com>
2020-07-28 04:35:08 +0900

    작성자 이미지 추가, 변수명 변경, 구분선 스타일 변경
    writer -> writerName (변경)
    writerImage (유저 이미지 변수 추가)
    hr 구분선 점선으로 스타일 변경

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,9 +3,18 @@
     <v-col cols="12">
       <h1>{{noticeHeader.title}}</h1>
       <hr />
-      <span>작성자:{{noticeHeader.writer}}</span>
-      <span>|작성일:{{noticeHeader.date}}</span>
-      <span>|조회수:{{noticeHeader.views}}</span>
+      <table>
+        <tr>
+          <td rowspan="2">
+            <img :src="noticeHeader.writerImage" width="50px" />
+          </td>
+          <td style="vertical-align:top;">작성자:{{noticeHeader.writerName}}</td>
+        </tr>
+        <tr style="vertical-align:bottom;">
+          <td>작성일:{{noticeHeader.date}}</td>
+          <td>|조회수:{{noticeHeader.views}}</td>
+        </tr>
+      </table>
     </v-col>
   </v-row>
 </template>
@@ -15,8 +24,15 @@ export default {
   props: {
     noticeHeader: {
       type: Object,
-      required: true
-    }
-  }
+      required: true,
+    },
+  },
 };
 </script>
+
+<style scoped>
+hr {
+  background-color: #fff;
+  border-top: 2px dashed #8c8b8b;
+}
+</style>
--- src/views/Notice.vue
+++ src/views/Notice.vue
@@ -19,9 +19,11 @@ export default {
     return {
       noticeHeader: {
         title: "제목",
-        writer: "홍길동",
+        writerName: "홍길동",
         date: "2020년 7월 14일",
         views: "999",
+        writerImage:
+          "https://previews.123rf.com/images/pedrolieb/pedrolieb1205/pedrolieb120500010/13528955-%EC%9D%B5%EB%AA%85-%EA%B7%B8%EB%A6%BC.jpg",
       },
       noticeBody: {
         mdText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
