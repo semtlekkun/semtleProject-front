64e3009b755834161825827a9d692c91d222ab1f
gunwoongPark <bung1438@gmail.com>
2020-07-28 04:12:45 +0900

    예제 변경

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -2,7 +2,7 @@
   <v-row>
     <v-col cols="12">
       <VueMarkDown>{{noticeBody.mdText}}</VueMarkDown>
-      <img :src="noticeBody.imagePath" alt="예지쨩 사진" width="300px" />
+      <img :src="noticeBody.imagePath" alt="Lena" width="300px" />
     </v-col>
   </v-row>
 </template>
@@ -13,11 +13,11 @@ export default {
   props: {
     noticeBody: {
       type: Object,
-      required: true
-    }
+      required: true,
+    },
   },
   components: {
-    VueMarkDown
-  }
+    VueMarkDown,
+  },
 };
 </script>
--- src/views/Notice.vue
+++ src/views/Notice.vue
@@ -18,15 +18,14 @@ export default {
   data() {
     return {
       noticeHeader: {
-        title: "제목제목제목제목제목제목",
+        title: "제목",
         writer: "홍길동",
         date: "2020년 7월 14일",
         views: "999",
       },
       noticeBody: {
         mdText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-        imagePath:
-          "https://upload.wikimedia.org/wikipedia/commons/b/bb/Hwang_Ye-ji_going_to_a_Music_Bank_recording_on_August_8%2C_2019.png",
+        imagePath: "https://upload.wikimedia.org/wikipedia/ko/2/24/Lenna.png",
       },
     };
   },
