fa6c8cfc8f788587b2f0b85c34d6be5aa303415d
gunwoongPark <bung1438@gmail.com>
2020-08-02 13:02:08 +0900

    양방향 바인딩
    폼에 적절한 데이터가 입력된 상태가 아닐 경우 예외처리 해야함

--- src/views/AdminMentor.vue
+++ src/views/AdminMentor.vue
@@ -1,34 +1,36 @@
 <template>
   <v-container>
     <v-row>
-      <v-col>간부 등록</v-col>
+      <v-col>역대 간부 등록</v-col>
     </v-row>
 
-    <v-row>
-      <v-col cols="1">
-        <v-text-field placeholder="활동 년도" solo></v-text-field>
-      </v-col>
+    <v-form>
+      <v-row>
+        <v-col>
+          <v-text-field placeholder="활동 년도" solo v-model="activeYear"></v-text-field>
+        </v-col>
 
-      <v-col cols="2">
-        <v-select :items="Semester" label="활동한 학기" solo></v-select>
-      </v-col>
+        <v-col>
+          <v-select :items="Semester" label="활동한 학기" solo v-model="Semester"></v-select>
+        </v-col>
 
-      <v-col cols="1">
-        <v-text-field placeholder="학번" solo></v-text-field>
-      </v-col>
-    </v-row>
+        <v-col>
+          <v-text-field placeholder="학번" solo v-model="studentId"></v-text-field>
+        </v-col>
+      </v-row>
 
-    <v-row>
-      <v-col>
-        <v-textarea solo label="내용"></v-textarea>
-      </v-col>
-    </v-row>
+      <v-row>
+        <v-col>
+          <v-textarea solo label="내용" v-model="contents"></v-textarea>
+        </v-col>
+      </v-row>
 
-    <v-row>
-      <v-col>
-        <v-btn block color="primary">등록</v-btn>
-      </v-col>
-    </v-row>
+      <v-row>
+        <v-col>
+          <v-btn block color="primary" type="submit" @click="checkForm">등록</v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
   </v-container>
 </template>
 
@@ -36,10 +38,15 @@
 export default {
   data() {
     return {
+      activeYear: "",
       Semester: ["1학기(여름학기)", "2학기(겨울학기)"],
+      studentId: "",
+      contents: "",
     };
   },
 
-  methods: {},
+  methods: {
+    checkForm() {},
+  },
 };
 </script>
