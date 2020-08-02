55834c57ea9b9b96aa36c48772042ee01be9090b
hayoungzzzzz <jhy901@naver.com>
2020-08-02 22:17:37 +0900

    LoginForm.vue 컴포넌트 제작.  LoginLogo.vue 컴포넌트 제작. Login.vue 페이지에 컴포넌트 부착 1. LoginForm.vue 화면사이즈 별 그리드 적용 , 아이디 비밀번호 입력 폼 제작 및 디자인 , 제출 버튼 디자인 및 최종 정렬 2. LoginLogo.vue 디자인에 따라 로그인 로고 제작. 3. Login.vue 페이지에 컴포넌트 부착. 메뉴바가 있을것을 대비하여 br태그 포함함.

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="LoginForm">
+    <v-form>
+      <v-layout row wrap class="mt-5 center">
+        <v-flex xs12 sm2 md2 lg1 class="text-md-right pr-5">
+          <p class="text">학번</p>
+        </v-flex>
+        <v-flex xs12 sm10 md5 lg3>
+          <input type="text" class="StudentNumberInput" />
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap class="mt-5 center">
+        <v-flex xs12 sm2 md2 lg1 class="text-md-right pr-5">
+          <p class="text">비밀번호</p>
+        </v-flex>
+        <v-flex xs12 sm10 md5 lg3>
+          <input type="password" class="PasswordInput" />
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap class="mt-10">
+        <v-flex xs6 sm4 md3 class="center">
+          <button type="submit" class="LoginButton">로그인</button>
+        </v-flex>
+      </v-layout>
+    </v-form>
+  </div>
+</template>
+<script>
+export default {
+  name: "LoginForm",
+};
+</script>
+<style scoped>
+.StudentNumberInput,
+.PasswordInput {
+  border: none;
+  background-color: rgb(233, 233, 233);
+  border-radius: 60px;
+  height: 60px;
+  width: 100%;
+  padding-left:1em;
+}
+.LoginForm {
+  margin: 0.3em;
+}
+.text {
+  line-height: 60px;
+  font-family:"Microsoft GothicNeo";
+  font-size:20px;
+  font-weight:bold;
+}
+.center {
+  margin: 0 auto;
+  justify-content: center;
+}
+
+.LoginButton {
+  border-radius: 60px;
+  border: none;
+  background-color: #2a404e;
+  width: 100%;
+  height: 60px;
+  color: white;
+  font-weight: bold;
+  font-size: 20px;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="login">
-  </div>
+  <v-container>
+  <br/><br/><br/><br/><br/><br/><br/>
+    <LoginLogo></LoginLogo>
+    <br/><br/>
+    <LoginForm></LoginForm>
+  </v-container>
 </template>
 
 <script>
+import LoginForm from "../components/LoginForm";
+import LoginLogo from "../components/LoginLogo";
 export default {
-  name: 'Login',
+  name: "Login",
   components: {
-  }
-}
+    LoginForm,
+    LoginLogo,
+  },
+};
 </script>
