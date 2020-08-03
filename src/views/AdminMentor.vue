64b7178efdd1a246d962f3cb3da2372a76c98f28
gunwoongPark <bung1438@gmail.com>
2020-08-04 00:19:44 +0900

    'v-form' 태그를 -> 'form' 태그로 변경할 수 있음

--- src/views/AdminMentor.vue
+++ src/views/AdminMentor.vue
@@ -1,28 +1,30 @@
 <template>
   <v-container>
-    <v-dialog v-model="dialog" width="500">
+    <v-dialog v-model="dialog" width="400" persistent>
       <template v-slot:activator="{ on, attrs }">
         <v-row>
-          <v-col>역대 간부 등록</v-col>
+          <v-col>
+            <h1>역대 간부(멘토) 등록</h1>
+          </v-col>
         </v-row>
-        <v-form @submit="checkForm">
+        <v-form @submit="checkForm" novalidate="true">
           <v-row>
             <v-col>
-              <v-text-field placeholder="활동 년도" solo v-model="activeYear"></v-text-field>
+              <v-text-field color="primary" placeholder="활동 년도 (ex : 2016)" v-model="activeYear"></v-text-field>
             </v-col>
 
             <v-col>
-              <v-select :items="Semester" label="활동한 학기" solo v-model="Semester"></v-select>
+              <v-select color="primary" v-model="Semester" :items="items" label="활동한 학기"></v-select>
             </v-col>
 
             <v-col>
-              <v-text-field placeholder="학번" solo v-model="studentId"></v-text-field>
+              <v-text-field color="primary" placeholder="학번 (ex : 20160450)" v-model="studentId"></v-text-field>
             </v-col>
           </v-row>
 
           <v-row>
             <v-col>
-              <v-textarea solo label="내용" v-model="contents"></v-textarea>
+              <v-textarea filled label="내용" color="primary" no-resize rows="20" v-model="contents"></v-textarea>
             </v-col>
           </v-row>
 
@@ -35,7 +37,15 @@
       </template>
 
       <v-card>
-        <v-card-text>test</v-card-text>
+        <v-card-title id="cardTitle">ERROR!</v-card-title>
+        <hr />
+        <br />
+        <v-card-text v-for="msg in errorMsg" :key="msg.index">
+          <span>-</span>
+          {{msg}}
+        </v-card-text>
+
+        <v-btn id="dialogBtn" @click="dialog = false" color="primary">확인</v-btn>
       </v-card>
     </v-dialog>
   </v-container>
@@ -43,22 +53,51 @@
 
 <script>
 export default {
-  data() {
-    return {
-      dialog: false,
+  data: () => ({
+    dialog: false,
 
-      activeYear: "",
-      Semester: ["1학기(여름학기)", "2학기(겨울학기)"],
-      studentId: "",
-      contents: "",
-    };
-  },
+    errorMsg: [],
+
+    items: ["1학기(여름학기)", "2학기(겨울학기)"],
+
+    activeYear: "",
+    Semester: "1학기(여름학기)",
+    studentId: "",
+    contents: "",
+  }),
 
   methods: {
     checkForm(e) {
       this.dialog = false;
+      this.errorMsg = [];
+      console.log(this.Semester);
+
+      if (this.activeYear.length != 4)
+        this.errorMsg.push("활동 년도를 정확하게 입력해주세요.");
+
+      if (this.studentId.length != 8)
+        this.errorMsg.push("학번을 정확하게 입력해주세요.");
+
+      if (this.contents === "")
+        this.errorMsg.push("간단하게라도 내용을 적어주세요.");
+
+      if (!this.errorMsg.length) return true;
+      else this.dialog = true;
+
       e.preventDefault();
     },
   },
 };
 </script>
+
+<style scoped>
+#dialogBtn {
+  position: absolute;
+  right: 5%;
+  bottom: 5%;
+}
+
+#cardTitle {
+  color: red;
+}
+</style>
