703274c973e57a1dd96b16e6e94130c21be5fce2
gunwoongPark <bung1438@gmail.com>
2020-08-05 07:11:31 +0900

    컴포넌트 파일 이름 변경

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <v-app>
     <v-content>
-      <MenuBar3 />
+      <MainMenuBar />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import MenuBar3 from "./components/MenuBar3.vue";
+import MainMenuBar from "./components/MainMenuBar.vue";
 
 export default {
   name: "App",
 
   components: {
-    MenuBar3,
+    MainMenuBar,
   },
 
   data: () => ({
