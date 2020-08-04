170d3f392f1ed4d003fb39812114d9c38cbcd008
gunwoongPark <bung1438@gmail.com>
2020-08-05 04:54:56 +0900

    카테고리 메뉴 구현

--- src/components/MenuBar3.vue
+++ src/components/MenuBar3.vue
@@ -4,7 +4,14 @@
       <v-col>
         <div id="top" @mouseover="logoHover" @mouseout="topOut">
           <img src="../assets/logo.png" width="150px" id="logo" />
-          <div id="box"></div>
+          <div id="box">
+            <div class="dummy1"></div>
+            <v-btn class="child white--text" text>공지사항</v-btn>
+            <v-btn class="child white--text" text>프로젝트</v-btn>
+            <v-btn class="child white--text" text>질의응답</v-btn>
+            <v-btn class="child white--text" text>로그인</v-btn>
+            <div class="dummy2"></div>
+          </div>
         </div>
       </v-col>
     </v-row>
@@ -18,20 +25,14 @@ export default {
   },
   methods: {
     logoHover() {
-      let browserWidth = document.body.clientWidth;
-      let rangeLogo = document.querySelector("#logo").getBoundingClientRect().x;
-
       let boxObj = document.querySelector("#box");
-
-      boxObj.style.width = (browserWidth - rangeLogo * 2).toString() + "px";
+      boxObj.style.width = "1200px";
       boxObj.style.transition = "1s";
-      console.log((browserWidth - rangeLogo).toString());
-      console.log(rangeLogo);
     },
 
     topOut() {
       let boxObj = document.querySelector("#box");
-      boxObj.style.width = "0%";
+      boxObj.style.width = "0";
       boxObj.style.transition = "1s";
     },
   },
@@ -57,10 +58,32 @@ export default {
   background: rgb(46, 117, 182);
   transition: 0.4s;
   margin-left: 4px;
+
+  border-radius: 20px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 #menuBar {
   position: sticky;
   top: 0;
+}
+
+.child {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+
+  font-weight: bold;
+}
+
+.dummy1 {
+  width: 150px;
+  height: 40px;
+}
+
+.dummy2 {
+  width: 0px;
+  height: 39.5px;
 }
 </style>
