3e1a942aee01a39e989f289b7037d007edb14978
gunwoongPark <bung1438@gmail.com>
2020-08-05 07:13:37 +0900

    기능 구현 완료 ㅎ

---
+++ src/components/MainMenuBar.vue
@@ -0,0 +1,187 @@
+<template>
+  <v-container id="container">
+    <v-row id="myrow">
+      <v-col>
+        <div id="top" @mouseover="menuOpen" @mouseout="menuClose">
+          <img src="../assets/logo.png" width="150px" id="logo" />
+          <div id="glassBox">
+            <div id="box">
+              <div class="dummy1"></div>
+              <MenuElement v-for="item in Attributes" :key="item.index" :Attribute="item" />
+              <div class="dummy2"></div>
+            </div>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import MenuElement from "./MenuElement.vue";
+export default {
+  components: {
+    MenuElement,
+  },
+  data() {
+    return {
+      controller: false,
+
+      Attributes: [
+        {
+          Items: [
+            { title: "Click Me" },
+            { title: "Click Me" },
+            { title: "Click Medskjflsdlkjfsdlkfjdsfljkdsfkljdsflkj" },
+            { title: "Click Me 2fdsifhsdakjfsdhlfsdkfjdlkfj" },
+          ],
+          Title: "공지사항",
+          Stop: this.controllStop,
+          Start: this.controllStart,
+        },
+
+        {
+          Items: [
+            { title: "Click Me" },
+            { title: "Click Me" },
+            { title: "Click Medskjflsdlkjfsdlkfjdsfljkdsfkljdsflkj" },
+            { title: "Click Me 2fdsifhsdakjfsdhlfsdkfjdlkfj" },
+          ],
+          Title: "프로젝트",
+          Stop: this.controllStop,
+          Start: this.controllStart,
+        },
+
+        {
+          Items: [
+            { title: "Click Me" },
+            { title: "Click Me" },
+            { title: "Click Medskjflsdlkjfsdlkfjdsfljkdsfkljdsflkj" },
+            { title: "Click Me 2fdsifhsdakjfsdhlfsdkfjdlkfj" },
+          ],
+          Title: "질의응답",
+          Stop: this.controllStop,
+          Start: this.controllStart,
+        },
+
+        {
+          Items: [
+            { title: "Click Me" },
+            { title: "Click Me" },
+            { title: "Click Medskjflsdlkjfsdlkfjdsfljkdsfkljdsflkj" },
+            { title: "Click Me 2fdsifhsdakjfsdhlfsdkfjdlkfj" },
+          ],
+          Title: "로그인",
+          Stop: this.controllStop,
+          Start: this.controllStart,
+        },
+      ],
+    };
+  },
+  methods: {
+    menuOpen() {
+      if (this.controller) {
+        let boxObj = document.querySelector("#glassBox");
+        boxObj.style.width = "1200px";
+        boxObj = document.querySelector("#box");
+        boxObj.style.width = "1200px";
+        boxObj.style.transition = "1s";
+      }
+    },
+
+    menuClose() {
+      if (this.controller) {
+        let boxObj = document.querySelector("#glassBox");
+        boxObj.style.width = "0px";
+        boxObj = document.querySelector("#box");
+        boxObj.style.width = "0px";
+        boxObj.style.transition = "1s";
+      }
+    },
+
+    controllStop() {
+      this.menuOpen();
+      this.controller = false;
+    },
+
+    controllStart() {
+      this.controller = true;
+      this.menuClose();
+    },
+
+    handleScroll() {
+      if ((window.scrollY || document.documentElement.scrollTop) < 10) {
+        this.controllStop();
+      } else {
+        this.controllStart();
+      }
+    },
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+};
+</script>
+
+<style scoped>
+#myrow {
+  position: sticky;
+  top: 2%;
+}
+
+#container {
+  height: 10000px;
+}
+
+#logo {
+  position: absolute;
+  top: 5px;
+  z-index: 100;
+}
+
+#box {
+  position: absolute;
+  top: 60.5px;
+  width: 1200px;
+  height: 39.5px;
+  background: rgb(46, 117, 182);
+  transition: 0.4s;
+  margin-left: 4px;
+
+  border-radius: 20px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+#glassBox {
+  width: 0px;
+  height: 150px;
+}
+
+#menuBar {
+  position: sticky;
+  top: 0;
+}
+
+.child {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+
+  font-weight: bold;
+}
+
+.dummy1 {
+  width: 150px;
+  height: 40px;
+}
+
+.dummy2 {
+  width: 0px;
+  height: 39.5px;
+}
+</style>
---
+++ src/components/MenuElement.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-menu offset-y open-on-hover>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn class="child white--text" text v-bind="attrs" v-on="on">{{Attribute.Title}}</v-btn>
+    </template>
+    <v-list>
+      <v-list-item
+        v-for="(item, index) in Attribute.Items"
+        :key="index"
+        @mouseover="Attribute.Stop"
+        @mouseout="Attribute.Start"
+      >
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  props: {
+    Attribute: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
