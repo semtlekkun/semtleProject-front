88a2111ecbdd3629cfd13017b21058d352cf5b7f
suhyeondev <contact.suhyeon@gmail.com>
2020-08-04 22:16:58 +0900

    2020.8.4 코드 정리

--- src/views/ProjectWrite.vue
+++ src/views/ProjectWrite.vue
@@ -1,74 +1,70 @@
 <template>
   <div id="projectWrite">
-    <v-app>
-      <v-content>
-        <v-container style="margin-top: 4rem; margin-bottom: 4rem">
-          <v-card>
-            <v-alert outlined color="#226db2">
-              <v-form>
-                <v-row>
-                  <v-col cols="12">
-                    <v-text-field label="제목" required></v-text-field>
-                  </v-col>
-                </v-row>
+    <v-container>
+      <v-card>
+        <v-alert outlined color="primary">
+          <v-form>
+            <v-row>
+              <v-col>
+                <v-text-field label="제목" required></v-text-field>
+              </v-col>
+            </v-row>
 
-                <v-row>
-                  <v-col cols="6">
-                    <v-text-field label="모집인원" required></v-text-field>
-                  </v-col>
-                  <v-col cols="6">
-                    <v-menu
-                      ref="menu"
-                      v-model="menu"
-                      :close-on-content-click="false"
-                      :return-value.sync="date"
-                      lazy
-                      transition="scale-transition"
-                      offset-y
-                      full-width
-                      min-width="290px"
-                    >
-                      <template v-slot:activator="{ on }">
-                        <v-text-field v-model="date" label="마감일" readonly v-on="on"></v-text-field>
-                      </template>
+            <v-row>
+              <v-col cols="6">
+                <v-text-field label="모집인원" required></v-text-field>
+              </v-col>
+              <v-col cols="6">
+                <v-menu
+                  ref="menu"
+                  v-model="menu"
+                  :close-on-content-click="false"
+                  :return-value.sync="date"
+                  lazy
+                  transition="scale-transition"
+                  offset-y
+                  full-width
+                  min-width="290px"
+                >
+                  <template v-slot:activator="{ on }">
+                    <v-text-field v-model="date" label="마감일" readonly v-on="on"></v-text-field>
+                  </template>
 
-                      <v-date-picker v-model="date" no-title scrollable>
-                        <v-spacer></v-spacer>
-                        <v-btn flat color="primary" @click="$refs.menu.save(date)">확인</v-btn>
-                      </v-date-picker>
-                    </v-menu>
-                  </v-col>
-                </v-row>
+                  <v-date-picker v-model="date" no-title scrollable>
+                    <v-spacer></v-spacer>
+                    <v-btn flat color="primary" @click="$refs.menu.save(date)">확인</v-btn>
+                  </v-date-picker>
+                </v-menu>
+              </v-col>
+            </v-row>
 
-                <v-row>
-                  <v-col cols="12">
-                    <v-textarea rows="7" auto-grow outlined label="내용"></v-textarea>
-                  </v-col>
-                </v-row>
+            <v-row>
+              <v-col>
+                <v-textarea rows="7" auto-grow outlined label="내용"></v-textarea>
+              </v-col>
+            </v-row>
 
-                <v-row>
-                  <v-col cols="12">
-                    <v-file-input label="파일 첨부" outlined v-model="chosenFile"></v-file-input>
-                  </v-col>
-                </v-row>
+            <v-row>
+              <v-col>
+                <v-file-input label="파일 첨부" outlined v-model="chosenFile"></v-file-input>
+              </v-col>
+            </v-row>
 
-                <v-row>
-                  <v-col cols="12">
-                    <v-btn color="primary">완료</v-btn>
-                  </v-col>
-                </v-row>
-              </v-form>
-            </v-alert>
-          </v-card>
-        </v-container>
-      </v-content>
-    </v-app>
+            <v-row>
+              <v-col>
+                <v-btn color="primary">완료</v-btn>
+              </v-col>
+            </v-row>
+          </v-form>
+        </v-alert>
+      </v-card>
+    </v-container>
   </div>
 </template>
 
 <script>
 export default {
-  data: () => ({})
+  data: () => ({}),
 };
 </script>
 
