39338ac4145073f9b9595b2f93ce5de7f106810a
gunwoongPark <bung1438@gmail.com>
2020-08-05 12:51:46 +0900

    최종 구현 완료
    스크롤이 최 상단에 위치할 경우 메뉴바 펼침
    스크롤이 최상단에서 내려갈 경우 메뉴바 닫힘
    메뉴바가 닫힌 상태에서 로고 호버 시 메뉴바 펼침
    펼쳐진 상태에서 위든 아래든 마우스 스크롤 시 닫힘
    
    버그 -> 메뉴바가 닫힌 상태에서 로고의 오른쪽에 버튼들이 뭉쳐짐 -> ref를 사용하여 자식측에서 지우는 함수를 호출하면 될 것으로보임.

--- src/components/MainMenuBar.vue
+++ src/components/MainMenuBar.vue
@@ -1,21 +1,17 @@
 <template>
   <v-container id="container">
-    <v-row id="myrow">
+    <v-row id="glue">
       <v-col>
-        <div id="top" @mouseover="menuOpen" @mouseout="menuClose">
-          <img src="../assets/logo.png" width="150px" id="logo" />
-          <div>
-            <div id="box">
-              <div class="dummy1"></div>
-              <MenuElement
-                class="test"
-                v-for="item in Attributes"
-                :key="item.index"
-                :Attribute="item"
-              />
-              <div class="dummy2"></div>
-            </div>
-          </div>
+        <img src="../assets/logo.png" width="150px" id="logo" @mouseover="menuOpen" />
+        <div id="box">
+          <div class="dummy1"></div>
+          <MenuElement
+            v-for="(item, index) in Attributes"
+            :key="index"
+            :Attribute="item"
+            ref="args"
+          />
+          <div class="dummy2"></div>
         </div>
       </v-col>
     </v-row>
@@ -30,102 +26,90 @@ export default {
   },
   data() {
     return {
+      // 현재 스크롤 위치와 호버에 대한 제어를 두기 위한 변수
       controller: false,
 
+      // 메뉴 버튼마다 들어갈 리스트 아이템과 버튼 제목
       Attributes: [
         {
           Items: [
             { title: "Click Me" },
             { title: "Click Me" },
-            { title: "Click Medskjflsdlkjfsdlkfjdsfljkdsfkljdsflkj" },
-            { title: "Click Me 2fdsifhsdakjfsdhlfsdkfjdlkfj" },
+            { title: "Click Me" },
+            { title: "Click Me" },
           ],
           Title: "공지사항",
-          Stop: this.controllStop,
-          Start: this.controllStart,
         },
 
         {
           Items: [
             { title: "Click Me" },
             { title: "Click Me" },
-            { title: "Click Medskjflsdlkjfsdlkfjdsfljkdsfkljdsflkj" },
-            { title: "Click Me 2fdsifhsdakjfsdhlfsdkfjdlkfj" },
+            { title: "Click Me" },
+            { title: "Click Me" },
           ],
           Title: "프로젝트",
-          Stop: this.controllStop,
-          Start: this.controllStart,
         },
 
         {
           Items: [
             { title: "Click Me" },
             { title: "Click Me" },
-            { title: "Click Medskjflsdlkjfsdlkfjdsfljkdsfkljdsflkj" },
-            { title: "Click Me 2fdsifhsdakjfsdhlfsdkfjdlkfj" },
+            { title: "Click Me" },
+            { title: "Click Me" },
           ],
           Title: "질의응답",
-          Stop: this.controllStop,
-          Start: this.controllStart,
         },
 
         {
           Items: [
             { title: "Click Me" },
             { title: "Click Me" },
-            { title: "Click Medskjflsdlkjfsdlkfjdsfljkdsfkljdsflkj" },
-            { title: "Click Me 2fdsifhsdakjfsdhlfsdkfjdlkfj" },
+            { title: "Click Me" },
+            { title: "Click Me" },
           ],
           Title: "로그인",
-          Stop: this.controllStop,
-          Start: this.controllStart,
         },
       ],
     };
   },
   methods: {
+    // 메뉴가 펼쳐지는 함수
     menuOpen() {
       if (this.controller) {
-        let topObj = document.querySelector("#top");
-        topObj.style.width = "1200px";
         let boxObj = document.querySelector("#box");
         boxObj.style.width = "1200px";
         boxObj.style.transition = "1s";
       }
     },
 
+    // 메뉴가 닫히는 함수
     menuClose() {
       if (this.controller) {
-        let topObj = document.querySelector("#top");
-        topObj.style.width = "0px";
         let boxObj = document.querySelector("#box");
         boxObj.style.width = "0px";
         boxObj.style.transition = "1s";
       }
     },
 
-    controllStop() {
-      this.menuOpen();
-      this.controller = false;
-    },
-
-    controllStart() {
-      this.controller = true;
-      this.menuClose();
-    },
-
+    // 스크롤 시 수행되는 함수
     handleScroll() {
       if ((window.scrollY || document.documentElement.scrollTop) < 10) {
-        this.controllStop();
+        this.menuOpen();
+        this.controller = false;
       } else {
-        this.controllStart();
+        this.controller = true;
+        this.menuClose();
       }
     },
   },
+
+  // 스크롤 시 수행되는 함수를 인스턴스 생성 시 호출
   created() {
     window.addEventListener("scroll", this.handleScroll);
   },
 
+  // 사용한 함수는 인스턴스 소멸 시 제거
   destroyed() {
     window.removeEventListener("scroll", this.handleScroll);
   },
@@ -133,12 +117,7 @@ export default {
 </script>
 
 <style scoped>
-#top {
-  width: 0px;
-  height: 200px;
-}
-
-#myrow {
+#glue {
   position: sticky;
   top: 2%;
 }
@@ -156,9 +135,9 @@ export default {
 #box {
   position: absolute;
   top: 60.5px;
+  background-color: rgb(46, 117, 182);
   width: 1200px;
   height: 39.5px;
-  background: rgb(46, 117, 182);
   transition: 0.4s;
   margin-left: 4px;
 
--- src/components/MenuElement.vue
+++ src/components/MenuElement.vue
@@ -1,17 +1,21 @@
 <template>
   <v-menu offset-y open-on-hover>
     <template v-slot:activator="{ on, attrs }">
-      <v-btn class="child white--text" text large v-bind="attrs" v-on="on">{{Attribute.Title}}</v-btn>
+      <v-btn
+        id="menuBtn"
+        class="child white--text"
+        text
+        large
+        v-bind="attrs"
+        v-on="on"
+      >{{Attribute.Title}}</v-btn>
     </template>
-    <v-list id="listContainer">
-      <v-list-item
-        v-for="(item, index) in Attribute.Items"
-        :key="index"
-        @mouseover="Attribute.Stop"
-        @mouseout="Attribute.Start"
-      >
-        <v-list-item-title>{{ item.title }}</v-list-item-title>
-      </v-list-item>
+    <v-list flat>
+      <v-list-item-group>
+        <v-list-item v-for="(item, index) in Attribute.Items" :key="index">
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list-item-group>
     </v-list>
   </v-menu>
 </template>
@@ -23,6 +27,16 @@ export default {
       type: Object,
       required: true,
     },
+  },
+  methods: {
+    // 메뉴가 접혀질 때 버튼들을 지우고싶지만 ref가 잘안됩니다...
+    // 버그는 메뉴가 접혀져 있을 때 로고의 오른쪽에 버튼들이 조금씩 뭉쳐있습니다.
+    // eraseBtn() {
+    //   document.querySelector("#menuBtn").style.display = "none";
+    // },
+    // drawBtn() {
+    //   document.querySelector("#menuBtn").style.display = "block";
+    // },
   },
 };
 </script>
