9abfbfd0d697520ecc7739a3a642612ba932cc07
suhyeondev <contact.suhyeon@gmail.com>
2020-08-05 16:15:12 +0900

    2020.8.5 content 길어지면 모양이 이상해지던 문제 해결

--- src/views/Members.vue
+++ src/views/Members.vue
@@ -2,7 +2,7 @@
   <div id="members">
     <v-container class="text-center">
       <v-row>
-        <v-col cols="12">
+        <v-col>
           <v-card>
             <v-alert outlined color="primary">
               <v-card-title>{{title}}</v-card-title>
@@ -41,38 +41,8 @@
                       <p class="content">{{content}}</p>
                     </v-responsive>
                   </v-col>
-                  <v-col cols="6" sm="3">
-                    <v-responsive
-                      class="text-center primary lighten-2 rounded-pill d-inline-flex align-center justify-center ma-3 pa-6"
-                    >
-                      <v-img
-                        class="rounded-circle"
-                        height="100"
-                        width="100"
-                        src="https://images.squarespace-cdn.com/content/v1/5d6d91d62a5c4f0001f7df63/1591250171050-GLE1I49UFP5BZEK2V0EP/ke17ZwdGBToddI8pDm48kF5u7Uw9XShestTm9iBrIh0UqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcYCt66E9QQuUlHQrS4Ab9bfbNxW7EAbf_wJZI0i6lk1RAMO5BZD-WTyqcA0wsxYW1/blog+banner+-+blackout.png"
-                      ></v-img>
-                      <p class="role">{{role}}</p>
-                      <p class="number">{{number}}</p>
-                      <p class="name">{{name}}</p>
-                      <p class="content">{{content}}</p>
-                    </v-responsive>
-                  </v-col>
-                  <v-col cols="6" sm="3">
-                    <v-responsive
-                      class="text-center primary lighten-2 rounded-pill d-inline-flex align-center justify-center ma-3 pa-6"
-                    >
-                      <v-img
-                        class="rounded-circle"
-                        height="100"
-                        width="100"
-                        src="https://images.unsplash.com/photo-1590272456521-1bbe160a18ce?ixlib=rb-1.2.1&w=1000&q=80"
-                      ></v-img>
-                      <p class="role">{{role}}</p>
-                      <p class="number">{{number}}</p>
-                      <p class="name">{{name}}</p>
-                      <p class="content">{{content}}</p>
-                    </v-responsive>
-                  </v-col>
+                  <v-col cols="6" sm="3"></v-col>
+                  <v-col cols="6" sm="3"></v-col>
                 </v-row>
 
                 <h1 class="term">2021년 1학기</h1>
@@ -193,22 +163,7 @@
                       <p class="content">{{content}}</p>
                     </v-responsive>
                   </v-col>
-                  <v-col cols="6" sm="3">
-                    <v-responsive
-                      class="text-center primary lighten-2 rounded-pill d-inline-flex align-center justify-center ma-3 pa-6"
-                    >
-                      <v-img
-                        class="rounded-circle"
-                        height="100"
-                        width="100"
-                        src="https://images.unsplash.com/photo-1590272456521-1bbe160a18ce?ixlib=rb-1.2.1&w=1000&q=80"
-                      ></v-img>
-                      <p class="role">{{role}}</p>
-                      <p class="number">{{number}}</p>
-                      <p class="name">{{name}}</p>
-                      <p class="content">{{content}}</p>
-                    </v-responsive>
-                  </v-col>
+                  <v-col cols="6" sm="3"></v-col>
                 </v-row>
               </v-card-text>
             </v-alert>
@@ -226,7 +181,7 @@ export default {
     role: "회장",
     number: "20210000",
     name: "홍길동",
-    content: `00 및 000`,
+    content: `00 및 000 및 00000 및 00000 및 00 및 000 및 00000 및 00000`,
   }),
 
   components: {},
@@ -256,5 +211,8 @@ export default {
 }
 .term {
   margin-top: 50px;
+}
+.content {
+  max-width: 10em;
 }
 </style>
