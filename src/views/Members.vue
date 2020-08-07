af3d415f955fb966c17075c1ffaedcda0c6010f2
suhyeondev <contact.suhyeon@gmail.com>
2020-08-07 22:56:42 +0900

    2020.8.7 디자인 수정(학기 왼쪽 정렬, 구분선 추가, 카드 디자인 변경) 및 테스트용 이미지/문구 변경

--- src/views/Members.vue
+++ src/views/Members.vue
@@ -1,174 +1,118 @@
 <template>
   <div id="members">
     <v-container class="text-center">
+      <h1 class="text-left">2021년 2학기</h1>
       <v-row>
-        <v-col>
-          <v-card>
-            <v-alert outlined color="primary">
-              <v-card-title>{{title}}</v-card-title>
-              <v-card-text color="primary">
-                <h1 class="mt-16">2021년 2학기</h1>
-                <v-row justify="center">
-                  <v-col cols="6" sm="3">
-                    <v-responsive
-                      class="text-center primary lighten-2 rounded-pill d-inline-flex align-center justify-center ma-3 pa-6"
-                    >
-                      <v-img
-                        class="rounded-circle"
-                        height="100"
-                        width="100"
-                        src="https://images.squarespace-cdn.com/content/v1/5d6d91d62a5c4f0001f7df63/1591250171050-GLE1I49UFP5BZEK2V0EP/ke17ZwdGBToddI8pDm48kF5u7Uw9XShestTm9iBrIh0UqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcYCt66E9QQuUlHQrS4Ab9bfbNxW7EAbf_wJZI0i6lk1RAMO5BZD-WTyqcA0wsxYW1/blog+banner+-+blackout.png"
-                      ></v-img>
-                      <p class="role">{{role}}</p>
-                      <p class="number">{{number}}</p>
-                      <p class="name">{{name}}</p>
-                      <p class="content">{{content}}</p>
-                    </v-responsive>
-                  </v-col>
-                  <v-col cols="6" sm="3">
-                    <v-responsive
-                      class="text-center primary lighten-2 rounded-pill d-inline-flex align-center justify-center ma-3 pa-6"
-                    >
-                      <v-img
-                        class="rounded-circle"
-                        height="100"
-                        width="100"
-                        src="https://images.unsplash.com/photo-1590272456521-1bbe160a18ce?ixlib=rb-1.2.1&w=1000&q=80"
-                      ></v-img>
-                      <p class="role">{{role}}</p>
-                      <p class="number">{{number}}</p>
-                      <p class="name">{{name}}</p>
-                      <p class="content">{{content}}</p>
-                    </v-responsive>
-                  </v-col>
-                  <v-col cols="6" sm="3"></v-col>
-                  <v-col cols="6" sm="3"></v-col>
-                </v-row>
-
-                <h1 class="mt-16">2021년 1학기</h1>
-                <v-row justify="center">
-                  <v-col cols="6" sm="3">
-                    <v-responsive
-                      class="text-center primary lighten-2 rounded-pill d-inline-flex align-center justify-center ma-3 pa-6"
-                    >
-                      <v-img
-                        class="rounded-circle"
-                        height="100"
-                        width="100"
-                        src="https://images.squarespace-cdn.com/content/v1/5d6d91d62a5c4f0001f7df63/1591250171050-GLE1I49UFP5BZEK2V0EP/ke17ZwdGBToddI8pDm48kF5u7Uw9XShestTm9iBrIh0UqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcYCt66E9QQuUlHQrS4Ab9bfbNxW7EAbf_wJZI0i6lk1RAMO5BZD-WTyqcA0wsxYW1/blog+banner+-+blackout.png"
-                      ></v-img>
-                      <p class="role">{{role}}</p>
-                      <p class="number">{{number}}</p>
-                      <p class="name">{{name}}</p>
-                      <p class="content">{{content}}</p>
-                    </v-responsive>
-                  </v-col>
-                  <v-col cols="6" sm="3">
-                    <v-responsive
-                      class="text-center primary lighten-2 rounded-pill d-inline-flex align-center justify-center ma-3 pa-6"
-                    >
-                      <v-img
-                        class="rounded-circle"
-                        height="100"
-                        width="100"
-                        src="https://images.unsplash.com/photo-1590272456521-1bbe160a18ce?ixlib=rb-1.2.1&w=1000&q=80"
-                      ></v-img>
-                      <p class="role">{{role}}</p>
-                      <p class="number">{{number}}</p>
-                      <p class="name">{{name}}</p>
-                      <p class="content">{{content}}</p>
-                    </v-responsive>
-                  </v-col>
-                  <v-col cols="6" sm="3">
-                    <v-responsive
-                      class="text-center primary lighten-2 rounded-pill d-inline-flex align-center justify-center ma-3 pa-6"
-                    >
-                      <v-img
-                        class="rounded-circle"
-                        height="100"
-                        width="100"
-                        src="https://images.squarespace-cdn.com/content/v1/5d6d91d62a5c4f0001f7df63/1591250171050-GLE1I49UFP5BZEK2V0EP/ke17ZwdGBToddI8pDm48kF5u7Uw9XShestTm9iBrIh0UqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcYCt66E9QQuUlHQrS4Ab9bfbNxW7EAbf_wJZI0i6lk1RAMO5BZD-WTyqcA0wsxYW1/blog+banner+-+blackout.png"
-                      ></v-img>
-                      <p class="role">{{role}}</p>
-                      <p class="number">{{number}}</p>
-                      <p class="name">{{name}}</p>
-                      <p class="content">{{content}}</p>
-                    </v-responsive>
-                  </v-col>
-                  <v-col cols="6" sm="3">
-                    <v-responsive
-                      class="text-center primary lighten-2 rounded-pill d-inline-flex align-center justify-center ma-3 pa-6"
-                    >
-                      <v-img
-                        class="rounded-circle"
-                        height="100"
-                        width="100"
-                        src="https://images.unsplash.com/photo-1590272456521-1bbe160a18ce?ixlib=rb-1.2.1&w=1000&q=80"
-                      ></v-img>
-                      <p class="role">{{role}}</p>
-                      <p class="number">{{number}}</p>
-                      <p class="name">{{name}}</p>
-                      <p class="content">{{content}}</p>
-                    </v-responsive>
-                  </v-col>
-                </v-row>
-
-                <h1 class="mt-16">2020년 2학기</h1>
-                <v-row justify="center">
-                  <v-col cols="6" sm="3">
-                    <v-responsive
-                      class="text-center primary lighten-2 rounded-pill d-inline-flex align-center justify-center ma-3 pa-6"
-                    >
-                      <v-img
-                        class="rounded-circle"
-                        height="100"
-                        width="100"
-                        src="https://images.squarespace-cdn.com/content/v1/5d6d91d62a5c4f0001f7df63/1591250171050-GLE1I49UFP5BZEK2V0EP/ke17ZwdGBToddI8pDm48kF5u7Uw9XShestTm9iBrIh0UqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcYCt66E9QQuUlHQrS4Ab9bfbNxW7EAbf_wJZI0i6lk1RAMO5BZD-WTyqcA0wsxYW1/blog+banner+-+blackout.png"
-                      ></v-img>
-                      <p class="role">{{role}}</p>
-                      <p class="number">{{number}}</p>
-                      <p class="name">{{name}}</p>
-                      <p class="content">{{content}}</p>
-                    </v-responsive>
-                  </v-col>
-                  <v-col cols="6" sm="3">
-                    <v-responsive
-                      class="text-center primary lighten-2 rounded-pill d-inline-flex align-center justify-center ma-3 pa-6"
-                    >
-                      <v-img
-                        class="rounded-circle"
-                        height="100"
-                        width="100"
-                        src="https://images.unsplash.com/photo-1590272456521-1bbe160a18ce?ixlib=rb-1.2.1&w=1000&q=80"
-                      ></v-img>
-                      <p class="role">{{role}}</p>
-                      <p class="number">{{number}}</p>
-                      <p class="name">{{name}}</p>
-                      <p class="content">{{content}}</p>
-                    </v-responsive>
-                  </v-col>
-                  <v-col cols="6" sm="3">
-                    <v-responsive
-                      class="text-center primary lighten-2 rounded-pill d-inline-flex align-center justify-center ma-3 pa-6"
-                    >
-                      <v-img
-                        class="rounded-circle"
-                        height="100"
-                        width="100"
-                        src="https://images.squarespace-cdn.com/content/v1/5d6d91d62a5c4f0001f7df63/1591250171050-GLE1I49UFP5BZEK2V0EP/ke17ZwdGBToddI8pDm48kF5u7Uw9XShestTm9iBrIh0UqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcYCt66E9QQuUlHQrS4Ab9bfbNxW7EAbf_wJZI0i6lk1RAMO5BZD-WTyqcA0wsxYW1/blog+banner+-+blackout.png"
-                      ></v-img>
-                      <p class="role">{{role}}</p>
-                      <p class="number">{{number}}</p>
-                      <p class="name">{{name}}</p>
-                      <p class="content">{{content}}</p>
-                    </v-responsive>
-                  </v-col>
-                  <v-col cols="6" sm="3"></v-col>
-                </v-row>
-              </v-card-text>
-            </v-alert>
+        <v-col cols="6" sm="3">
+          <v-card class="pa-10">
+            <v-img
+              src="https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg"
+            ></v-img>
+            <p class="role">{{role}}</p>
+            <p class="number">{{number}}</p>
+            <p class="name">{{name}}</p>
+            <p class="content">{{content}}</p>
           </v-card>
         </v-col>
+        <v-col cols="6" sm="3">
+          <v-card class="pa-10">
+            <v-img
+              src="https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg"
+            ></v-img>
+            <p class="role">{{role}}</p>
+            <p class="number">{{number}}</p>
+            <p class="name">{{name}}</p>
+            <p class="content">{{content}}</p>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <v-divider class="mt-16"></v-divider>
+      <h1 class="mt-5 text-left">2021년 1학기</h1>
+      <v-row>
+        <v-col cols="6" sm="3">
+          <v-card class="pa-10">
+            <v-img
+              src="https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg"
+            ></v-img>
+            <p class="role">{{role}}</p>
+            <p class="number">{{number}}</p>
+            <p class="name">{{name}}</p>
+            <p class="content">{{content}}</p>
+          </v-card>
+        </v-col>
+        <v-col cols="6" sm="3">
+          <v-card class="pa-10">
+            <v-img
+              src="https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg"
+            ></v-img>
+            <p class="role">{{role}}</p>
+            <p class="number">{{number}}</p>
+            <p class="name">{{name}}</p>
+            <p class="content">{{content}}</p>
+          </v-card>
+        </v-col>
+        <v-col cols="6" sm="3">
+          <v-card class="pa-10">
+            <v-img
+              src="https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg"
+            ></v-img>
+            <p class="role">{{role}}</p>
+            <p class="number">{{number}}</p>
+            <p class="name">{{name}}</p>
+            <p class="content">{{content}}</p>
+          </v-card>
+        </v-col>
+        <v-col cols="6" sm="3">
+          <v-card class="pa-10">
+            <v-img
+              src="https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg"
+            ></v-img>
+            <p class="role">{{role}}</p>
+            <p class="number">{{number}}</p>
+            <p class="name">{{name}}</p>
+            <p class="content">{{content}}</p>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <v-divider class="mt-16"></v-divider>
+      <h1 class="mt-5 text-left">2020년 2학기</h1>
+      <v-row>
+        <v-col cols="6" sm="3">
+          <v-card class="pa-10">
+            <v-img
+              src="https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg"
+            ></v-img>
+            <p class="role">{{role}}</p>
+            <p class="number">{{number}}</p>
+            <p class="name">{{name}}</p>
+            <p class="content">{{content}}</p>
+          </v-card>
+        </v-col>
+        <v-col cols="6" sm="3">
+          <v-card class="pa-10">
+            <v-img
+              src="https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg"
+            ></v-img>
+            <p class="role">{{role}}</p>
+            <p class="number">{{number}}</p>
+            <p class="name">{{name}}</p>
+            <p class="content">{{content}}</p>
+          </v-card>
+        </v-col>
+        <v-col cols="6" sm="3">
+          <v-card class="pa-10">
+            <v-img
+              src="https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg"
+            ></v-img>
+            <p class="role">{{role}}</p>
+            <p class="number">{{number}}</p>
+            <p class="name">{{name}}</p>
+            <p class="content">{{content}}</p>
+          </v-card>
+        </v-col>
+        <v-col cols="6" sm="3"></v-col>
       </v-row>
     </v-container>
   </div>
@@ -177,11 +121,10 @@
 <script>
 export default {
   data: () => ({
-    title: "역대 간부",
     role: "회장",
     number: "20210000",
-    name: "홍길동",
-    content: `00 및 000 및 00000 및 00000 및 00 및 000 및 00000 및 00000`,
+    name: "김이름",
+    content: `내용내용내용내용내용내용내용내용`,
   }),
 
   components: {},
@@ -189,27 +132,26 @@ export default {
 </script>
 
 <style>
+.v-image__image {
+  border-radius: 5px;
+}
 .v-application p {
   margin-bottom: 0;
-  color: black;
 }
 .role,
-.number {
+.number,
+.content {
   font-size: 0.9em;
 }
 .role {
   color: #727272 !important;
-  margin-top: 10px;
+  margin-top: 20px;
 }
 .name {
   font-size: 1.1em;
   font-weight: 500;
 }
 .content {
-  font-size: 0.7em;
   color: #727272 !important;
-}
-.content {
-  max-width: 10em;
 }
 </style>
