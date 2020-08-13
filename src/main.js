5c752c4c6bac5ae51dacce978cfe8b825100dc81
taese0ng <taese0ng@naver.com>
2020-08-13 16:58:52 +0900

    2020/08/13 axios 설치완료 main에 사용법 적음

--- src/main.js
+++ src/main.js
@@ -4,8 +4,24 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios)
+//Usage
+// Vue.axios.get(api).then((response) => {
+//   console.log(response.data)
+// })
+ 
+// this.axios.get(api).then((response) => {
+//   console.log(response.data)
+// })
+ 
+// this.$http.get(api).then((response) => {
+//   console.log(response.data)
+// })
 
 new Vue({
   router,
