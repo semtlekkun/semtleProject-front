6ba433d18ededde396ff043566919e6937c74094
gunwoongPark <bung1438@gmail.com>
2020-08-13 22:05:18 +0900

    views 디렉토리 페이지 생성 및 라우터 연결

--- src/views/AdminMentor.vue
+++ src/views/AdminMentor.vue
@@ -1,90 +1,71 @@
 <template>
   <v-container>
-    <v-dialog v-model="dialog" width="400" persistent>
-      <template v-slot:activator="{ on, attrs }">
-        <v-row>
-          <v-col>
-            <h1>역대 간부(회장, 부회장, 멘토) 등록</h1>
-          </v-col>
-        </v-row>
-        <v-form @submit="checkForm" novalidate="true">
-          <v-row>
-            <v-col>
-              <v-text-field
-                color="primary"
-                placeholder="활동 년도 (ex : 2016)"
-                v-model="activeYear"
-                @keypress="checkNumber"
-                @keyup="checkHan"
-              ></v-text-field>
-            </v-col>
+    <v-row>
+      <v-col>
+        <h1>역대 간부(회장, 부회장, 멘토) 등록</h1>
+      </v-col>
+    </v-row>
 
-            <v-col>
-              <v-select color="primary" v-model="Semester" :items="SemesterItems" label="활동한 학기"></v-select>
-            </v-col>
-          </v-row>
+    <v-row>
+      <v-col>
+        <v-text-field
+          color="primary"
+          placeholder="활동 년도 (ex : 2016)"
+          v-model="activeYear"
+          @keypress="checkNumber"
+          @keyup="checkHan"
+        ></v-text-field>
+      </v-col>
 
-          <v-row>
-            <v-col>
-              <v-text-field
-                color="primary"
-                placeholder="학번 (ex : 20160450)"
-                v-model="studentId"
-                @keypress="checkNumber"
-                @keyup="checkHan"
-              ></v-text-field>
-            </v-col>
-            <v-col>
-              <v-select color="primary" v-model="Position" :items="PositionItems" label="직책"></v-select>
-            </v-col>
-          </v-row>
+      <v-col>
+        <v-select color="primary" v-model="Semester" :items="SemesterItems" label="활동한 학기"></v-select>
+      </v-col>
+    </v-row>
 
-          <v-row>
-            <v-col>
-              <v-select
-                v-if="Position==='멘토'"
-                color="primary"
-                v-model="Subject"
-                :items="MentorSubject"
-                label="과목"
-              ></v-select>
-            </v-col>
-          </v-row>
+    <v-row>
+      <v-col>
+        <v-text-field
+          color="primary"
+          placeholder="학번 (ex : 20160450)"
+          v-model="studentId"
+          @keypress="checkNumber"
+          @keyup="checkHan"
+        ></v-text-field>
+      </v-col>
+      <v-col>
+        <v-select color="primary" v-model="Position" :items="PositionItems" label="직책"></v-select>
+      </v-col>
+    </v-row>
 
-          <v-row>
-            <v-col>
-              <v-textarea filled label="내용" color="primary" no-resize rows="19" v-model="contents"></v-textarea>
-            </v-col>
-          </v-row>
+    <v-row>
+      <v-col>
+        <v-select
+          v-if="Position==='멘토'"
+          color="primary"
+          v-model="Subject"
+          :items="MentorSubject"
+          label="과목"
+        ></v-select>
+      </v-col>
+    </v-row>
 
-          <v-row>
-            <v-col>
-              <v-btn block color="primary" type="submit" v-bind="attrs" v-on="on">등록</v-btn>
-            </v-col>
-          </v-row>
-        </v-form>
-      </template>
+    <v-row>
+      <v-col>
+        <v-textarea filled label="내용" color="primary" no-resize rows="19" v-model="contents"></v-textarea>
+      </v-col>
+    </v-row>
 
-      <v-card>
-        <v-card-title id="cardTitle">ERROR!</v-card-title>
-        <hr />
-        <br />
-        <v-card-text v-for="msg in errorMsg" :key="msg.index">
-          <span>-</span>
-          {{msg}}
-        </v-card-text>
-
-        <v-btn id="dialogBtn" @click="dialog = false" color="primary">확인</v-btn>
-      </v-card>
-    </v-dialog>
+    <v-row>
+      <v-col>
+        <v-btn block color="primary" type="submit" @click="sendForm">등록</v-btn>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
   data: () => ({
-    dialog: false,
-
     errorMsg: [],
 
     SemesterItems: ["1학기 (여름학기)", "2학기 (겨울학기)"],
@@ -110,9 +91,7 @@ export default {
 
   methods: {
     checkForm(e) {
-      this.dialog = false;
       this.errorMsg = [];
-      console.log(this.Semester);
 
       if (this.activeYear.length != 4)
         this.errorMsg.push("활동 년도를 정확하게 입력해주세요.");
@@ -142,6 +121,71 @@ export default {
       var keyID = e.which ? e.which : e.keyCode;
       if (keyID == 8 || keyID == 46 || keyID == 37 || keyID == 39) return;
       else e.target.value = e.target.value.replace(/[^0-9]/g, "");
+    },
+
+    sendForm() {
+      this.errorMsg = [];
+
+      if (this.activeYear.length != 4)
+        this.errorMsg.push("활동 년도를 정확하게 입력해주세요.");
+
+      if (this.Semester === "")
+        this.errorMsg.push("활동한 학기를 선택해주세요.");
+
+      if (this.studentId.length != 8)
+        this.errorMsg.push("학번을 정확하게 입력해주세요.");
+
+      if (this.Position === "") this.errorMsg.push("직책을 선택해주세요.");
+
+      if (this.contents === "")
+        this.errorMsg.push("간단하게라도 내용을 적어주세요.");
+
+      if (this.errorMsg.length !== 0) {
+        let errString = "";
+        for (let idx = 0; idx < this.errorMsg.length; ++idx) {
+          errString = errString.concat(this.errorMsg[idx]);
+
+          if (idx === this.errorMsg.length - 1) {
+            break;
+          }
+          errString = errString.concat("\n");
+        }
+
+        // console.log(sessionStorage.getItem("token"));
+
+        alert(errString);
+      } else {
+        let sendObj = {
+          activeYear: this.activeYear,
+          season: this.Semester,
+          position: this.Position,
+          language: this.Subject,
+          studentCode: this.studentId,
+          contents: this.contents,
+        };
+
+        if (this.Position !== "멘토") {
+          sendObj.Subject = "";
+        }
+
+        let config = {
+          headers: { token: sessionStorage.getItem("token") },
+        };
+
+        this.axios
+          .post("http://49.50.166.64/api/management/input", sendObj, config)
+          .then((res) => {
+            if (res.status === 200) {
+              alert("등록 성공!");
+              location.reload(true);
+            } else {
+              alert("실패!");
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
     },
   },
 };
---
+++ src/views/AdminModify.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col></v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
