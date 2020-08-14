c530c976397a67d3433c56672173d8d120e51d79
KDH20160140 <bzidol@naver.com>
2020-08-14 13:28:10 +0900

    관리자 - 회원관리 통신 완료

--- src/views/AdminMember.vue
+++ src/views/AdminMember.vue
@@ -1,6 +1,5 @@
 <template>
-    <v-container>
-
+    <v-container id="container">
         <v-row>
         <v-col cols="12">
             <!-- :items="student" = 테이블 내용을 student(배열)로 채움. -->
@@ -57,17 +56,26 @@
                     <v-card-text>
                         <v-container>
                         <v-row>
-                            <v-col cols="12" sm="6" md="4">
-                            <v-text-field v-model="editedItem.studentCode" label="학번"></v-text-field>
+                            <v-col cols="12">
+                            <v-text-field
+                                type="number" 
+                                v-model="editedItem._id" 
+                                label="학번"
+                                :rules="idRule"
+                            ></v-text-field>
                             </v-col>
-                            <v-col cols="12" sm="6" md="4">
-                            <v-text-field v-model="editedItem.name" label="이름"></v-text-field>
+                            <v-col cols="12">
+                            <v-text-field 
+                                v-model="editedItem.name" 
+                                label="이름">
+                            </v-text-field>
                             </v-col>
-                            <v-col cols="12" sm="6" md="4">
-                            <v-text-field v-model="editedItem.phoneNum" label="전화번호"></v-text-field>
-                            </v-col>
-                            <v-col cols="12" sm="6" md="4">
-                            <v-text-field v-model="editedItem.password" label="비밀번호"></v-text-field>
+                            <v-col cols="12">
+                            <v-text-field 
+                                v-model="editedItem.phoneNum" 
+                                label="전화번호"
+                                type="">
+                            </v-text-field>
                             </v-col>
                         </v-row>
                         </v-container>
@@ -83,25 +91,7 @@
                 </v-toolbar>
             </template>
 
-            <!-- 학번 -->
-            <template v-slot:item.studentCode="{ item }">
-                <VueMarkdown style="margin-top:10px">{{item.studentCode}}</VueMarkdown>
-            </template>
 
-            <!-- 이름 -->
-            <template v-slot:item.name="{ item }">
-                <VueMarkdown style="margin-top:10px">{{item.name}}</VueMarkdown>
-            </template>
-
-            <!-- 전화번호 -->
-            <template v-slot:item.phoneNum="{ item }">
-                <VueMarkdown style="margin-top:10px">{{item.phoneNum}}</VueMarkdown>
-            </template>
-
-            <!-- 비밀번호 -->
-            <template v-slot:item.password="{ item }">
-               <VueMarkdown style="margin-top:10px">{{item.password}}</VueMarkdown>
-            </template>
 
             <!-- 수정, 이미지는 정렬 불가능 -->
             <!-- 수정기능 -->
@@ -145,11 +135,6 @@
                     ></v-img>
                 </div>
             </template>
-
-            <!-- 테스트용// 데이터 없는 경우 테스트 데이터 생성  -->
-            <template v-slot:no-data>
-                <v-btn color="primary" @click="initialize">Reset</v-btn>
-            </template>
             </v-data-table>
         </v-col>
         </v-row>
@@ -157,54 +142,67 @@
 </template>
 
 <script>
-    import VueMarkdown from 'vue-markdown';
+    //import VueMarkdown from 'vue-markdown';
+    //할일 : 전화번호 입력폼 맞추기
+    //학번 : 8자리 맞추기
+    //입력값 확인해서 해당하는 폼에 안맞으면 추가 X
+    //입력값 확인해서 없으면 추가 X
+    //세션값 사라지면 없다고 메세지
+    //Notice 에 스낵바 컴퓨터 화면에서 상단으로 안올라가는 버그 수정
+
+    //세션값이 없으면 표내용을 가져올수 없음 -> 아무데이터도 안보임.
     export default {
         components: {
-            VueMarkdown
+            //VueMarkdown
         },
         data: () => ({
+        //학번 입력값 확인
+        idRule: [
+            v => !!v || '입력값이 없습니다.',
+            v => (v.length === 8) || '학번은 8자리로 입력되어야 합니다.',
+        ],
+        //이름 입력값 확인
+        //전화번호 입력값 확인
         search:'',
         dialog: false,
         update : "2020.07.29",
+        token :'',
+
+        //여기에 토큰 저장해서 넘김.
+        head : '',
         headers: [
             {
             text: '학번',
             align: 'start',
             sortable: true,
-            value: 'studentCode',
+            value: '_id',
             },
             { text: '이름', value: 'name' },
             { text: '전화번호', value: 'phoneNum' },
-            { text: '비밀번호', value: 'password' },
             { text: '사진', value: 'image', sortable: false },
             { text: '수정', value: 'actions', sortable: false },
         ],
 
         //선택된 값 저장 배열
         selected: [],
+
         //학생 정보를 저장할 배열
+        //여기에 서버에서 불러온 정보를
         student: [],
-        //검색 범위
-        slots: [
-          "학번",
-          "이름",
-          "전화번호",
-          "비밀번호",
-        ],
 
         editedIndex: -1,
+
+        //_id -> 학번
         editedItem: {
-            studentCode: '',
+            _id: '',
             name: '',
             phoneNum: '',
-            password: '',
             image: '',
         },
         defaultItem: {
-            studentCode: '',
+            _id: '',
             name: '',
             phoneNum: '',
-            password: '',
             image: '',
         },
         }),
@@ -222,31 +220,129 @@
         },
 
         created () {
-        this.initialize()
+            //this.initialize()
+        },
+
+        //마운티드에서 부착.
+        beforeMount() {
+            this.check();
         },
 
         methods: {
-        //표에 들어갈 내용 초기화.
-        initialize () {
+        //-----
+        //통신
+        //조회
+        //res.status === 200 인게 성공한경우.
+        check() {
+            //이게 참이면 관리자
+            //토큰값 가져와서 저장한다음에 넘겨줌
+           if(sessionStorage.getItem("admin")){
+                this.getData();
+           }
+           //아니면 관리자 아님.
+           else {
+                //container -> display:none?
+                console.log("관리자 아님")
+           }
+        },
 
-            //학생 배열에 들어갈 정보값
-            this.student = [
-            {
-                studentCode: 20160140,
-                name:"김도현",
-                phoneNum: "01077935630",
-                password: "1234",
-                //테스트
-                image: "https://cdn.vuetifyjs.com/images/parallax/material2.jpg",
-            },
-            {
-                studentCode: 20141230,
-                name:"이름1",
-                phoneNum: "01077935630",
-                password: "1234",
-                image: '',
-            },
-            ]
+        //get
+        getData(){
+            let config = {
+                headers : {
+                    token : sessionStorage.getItem("token")
+                }
+            }
+
+            this.axios.get("http://49.50.166.64/api/student/list/1", config)
+            .then((res) => {
+                console.log("성공");
+                //console.log(res);
+
+                //모든 학생정보를 가져와서 student 에 저장.
+                //저장하면 알아서 다 뜸.
+                this.student = res.data.students;
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        },
+        //수정 put
+        modiDate(modidata){
+            let config = {
+                headers : {
+                    "token" : sessionStorage.getItem("token")
+                }
+            }
+            let data = {
+                studentCode : modidata._id,
+                phoneNum : modidata.phoneNum,
+                name : modidata.name
+            }
+            
+            this.axios.put("http://49.50.166.64/api/student/update",data, config)
+            .then((res) => {
+                console.log(res);
+                console.log("put 성공");
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        },
+
+        //추가 post
+        addData(adddata){
+            let config = {
+                headers : {
+                    "token" : sessionStorage.getItem("token")
+                }
+            }
+            let data = {
+                studentCode : adddata._id,
+                phoneNum : adddata.phoneNum,
+                name : adddata.name
+            }
+            this.axios.post("http://49.50.166.64/api/student/input",data,config)
+            .then((res) => {
+                console.log(res);
+                console.log("post 성공");
+
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        },
+        //삭제 delete
+        delDate(id){
+            this.axios.delete("http://49.50.166.64/api/student/delete",{
+            headers: {
+                    token: sessionStorage.getItem("token")
+                },
+                data: {
+                    studentCode : id
+                }
+            })
+            .then((res) => {
+                console.log("delete 성공");
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        },
+        //-----
+
+        //보낼값, config
+        //통신 끝
+
+        //수정 -> 해당 회원 삭제시 실제로 삭제가 이뤄지는 부분
+        del(index) {
+            //디비로 삭제 요청
+
+            this.delDate(this.student[index]._id);
+
+            //배열에서 삭제
+            this.student.splice(index, 1)
         },
 
         //Action 에서 수정하는 부분
@@ -262,7 +358,7 @@
         //Action 에서 삭제하는 부분
         deleteItem (item) {
             const index = this.student.indexOf(item)
-            confirm('삭제하시겠습니까?') && this.student.splice(index, 1)
+            confirm('삭제하시겠습니까?') && this.del(index);
         },
 
         //수정, 삭제 할때 뜨는 창에서 닫는 부분
@@ -275,11 +371,25 @@
         },
 
         //수정, 삭제 할때 뜨는 창에서 저장하는 부분
+
+        //여기서 put, delete ㄱㄱ
         save () {
             if (this.editedIndex > -1) {
-            Object.assign(this.student[this.editedIndex], this.editedItem)
-            } else {
-            this.student.push(this.editedItem)
+                Object.assign(this.student[this.editedIndex], this.editedItem)
+                if(this.student[this.editedIndex]._id.length <= 0) {
+                    console.log("학번이 없음.");
+                    return;
+                }
+                //put
+                this.modiDate(this.student[this.editedIndex]);
+                //console.log("수정");
+            } 
+            else {
+                this.student.push(this.editedItem)
+                const index = this.student.indexOf(this.editedItem)
+                //post
+                this.addData(this.student[index]);
+                //console.log("추가");
             }
             this.close()
         },
