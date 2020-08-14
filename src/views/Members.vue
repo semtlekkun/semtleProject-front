cfb0a27b6c6b98d0390eeab451b53d3f34d3b464
suhyeondev <contact.suhyeon@gmail.com>
2020-08-14 21:26:09 +0900

    2020.8.14. get 기능 구현

--- src/views/Members.vue
+++ src/views/Members.vue
@@ -1,115 +1,97 @@
 <template>
   <div id="members">
     <v-container class="text-center">
-      <h1 class="text-left">2021년 2학기</h1>
+      <h1 class="text-left">{{season}}</h1>
       <v-row>
         <v-col cols="6" sm="3">
           <v-card class="pa-10">
-            <v-img
-              src="https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg"
-            ></v-img>
-            <p class="role">{{role}}</p>
-            <p class="number">{{number}}</p>
+            <img v-bind:src="image" />
+            <p class="role">{{managementList}}</p>
+            <p class="number">{{studentCode}}</p>
             <p class="name">{{name}}</p>
-            <p class="content">{{content}}</p>
+            <p class="contents">{{contents}}</p>
           </v-card>
         </v-col>
         <v-col cols="6" sm="3">
           <v-card class="pa-10">
-            <v-img
-              src="https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg"
-            ></v-img>
-            <p class="role">{{role}}</p>
-            <p class="number">{{number}}</p>
+            <img v-bind:src="image" />
+            <p class="role">{{managementList}}</p>
+            <p class="studentCode">{{studentCode}}</p>
             <p class="name">{{name}}</p>
-            <p class="content">{{content}}</p>
+            <p class="contents">{{contents}}</p>
           </v-card>
         </v-col>
       </v-row>
 
       <v-divider class="mt-16"></v-divider>
-      <h1 class="mt-5 text-left">2021년 1학기</h1>
+      <h1 class="mt-5 text-left">{{season}}</h1>
       <v-row>
         <v-col cols="6" sm="3">
           <v-card class="pa-10">
-            <v-img
-              src="https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg"
-            ></v-img>
-            <p class="role">{{role}}</p>
-            <p class="number">{{number}}</p>
+            <img v-bind:src="image" />
+            <p class="role">{{managementList}}</p>
+            <p class="studentCode">{{studentCode}}</p>
             <p class="name">{{name}}</p>
-            <p class="content">{{content}}</p>
+            <p class="contents">{{contents}}</p>
           </v-card>
         </v-col>
         <v-col cols="6" sm="3">
           <v-card class="pa-10">
-            <v-img
-              src="https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg"
-            ></v-img>
-            <p class="role">{{role}}</p>
-            <p class="number">{{number}}</p>
+            <img v-bind:src="image" />
+            <p class="role">{{managementList}}</p>
+            <p class="studentCode">{{studentCode}}</p>
             <p class="name">{{name}}</p>
-            <p class="content">{{content}}</p>
+            <p class="contents">{{contents}}</p>
           </v-card>
         </v-col>
         <v-col cols="6" sm="3">
           <v-card class="pa-10">
-            <v-img
-              src="https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg"
-            ></v-img>
-            <p class="role">{{role}}</p>
-            <p class="number">{{number}}</p>
+            <img v-bind:src="image" />
+            <p class="role">{{managementList}}</p>
+            <p class="studentCode">{{studentCode}}</p>
             <p class="name">{{name}}</p>
-            <p class="content">{{content}}</p>
+            <p class="contents">{{contents}}</p>
           </v-card>
         </v-col>
         <v-col cols="6" sm="3">
           <v-card class="pa-10">
-            <v-img
-              src="https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg"
-            ></v-img>
-            <p class="role">{{role}}</p>
-            <p class="number">{{number}}</p>
+            <img v-bind:src="image" />
+            <p class="role">{{managementList}}</p>
+            <p class="studentCode">{{studentCode}}</p>
             <p class="name">{{name}}</p>
-            <p class="content">{{content}}</p>
+            <p class="contents">{{contents}}</p>
           </v-card>
         </v-col>
       </v-row>
 
       <v-divider class="mt-16"></v-divider>
-      <h1 class="mt-5 text-left">2020년 2학기</h1>
+      <h1 class="mt-5 text-left">{{season}}</h1>
       <v-row>
         <v-col cols="6" sm="3">
           <v-card class="pa-10">
-            <v-img
-              src="https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg"
-            ></v-img>
-            <p class="role">{{role}}</p>
-            <p class="number">{{number}}</p>
+            <img v-bind:src="image" />
+            <p class="role">{{managementList}}</p>
+            <p class="studentCode">{{studentCode}}</p>
             <p class="name">{{name}}</p>
-            <p class="content">{{content}}</p>
+            <p class="contents">{{contents}}</p>
           </v-card>
         </v-col>
         <v-col cols="6" sm="3">
           <v-card class="pa-10">
-            <v-img
-              src="https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg"
-            ></v-img>
-            <p class="role">{{role}}</p>
-            <p class="number">{{number}}</p>
+            <img v-bind:src="image" />
+            <p class="role">{{managementList}}</p>
+            <p class="studentCode">{{studentCode}}</p>
             <p class="name">{{name}}</p>
-            <p class="content">{{content}}</p>
+            <p class="contents">{{contents}}</p>
           </v-card>
         </v-col>
         <v-col cols="6" sm="3">
           <v-card class="pa-10">
-            <v-img
-              src="https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg"
-            ></v-img>
-            <p class="role">{{role}}</p>
-            <p class="number">{{number}}</p>
+            <img v-bind:src="image" />
+            <p class="role">{{managementList}}</p>
+            <p class="studentCode">{{studentCode}}</p>
             <p class="name">{{name}}</p>
-            <p class="content">{{content}}</p>
+            <p class="contents">{{contents}}</p>
           </v-card>
         </v-col>
         <v-col cols="6" sm="3"></v-col>
@@ -120,27 +102,49 @@
 
 <script>
 export default {
-  data: () => ({
-    role: "회장",
-    number: "20210000",
-    name: "김이름",
-    content: `내용내용내용내용내용내용내용내용`,
-  }),
+  data() {
+    return {
+      season: "2021년 1학기",
+      managementList: "회장",
+      studentCode: "20210000",
+      name: "김이름",
+      contents: `내용내용내용내용내용내용내용내용`,
+      image:
+        "https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg",
+    };
+  },
 
-  components: {},
+  mounted() {
+    console.log("ㄹㄹㄹ");
+    this.axios
+      .get("http://49.50.166.64/api/management/list")
+      .then((res) => {
+        console.log(res);
+        this.season = res.season;
+        this.studentCode = res.studentCode;
+        this.name = res.name;
+        this.image = res.image;
+        this.contents = res.contents;
+        this.managementList = res.managementList;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
 };
 </script>
 
 <style>
-.v-image__image {
+img {
   border-radius: 5px;
+  width: 100%;
 }
 .v-application p {
   margin-bottom: 0;
 }
 .role,
 .number,
-.content {
+.contents {
   font-size: 0.9em;
 }
 .role {
@@ -151,7 +155,7 @@ export default {
   font-size: 1.1em;
   font-weight: 500;
 }
-.content {
+.contents {
   color: #727272 !important;
 }
 </style>
