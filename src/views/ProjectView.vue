a1e47e37097a58603a73cd2a860b61ee87dc46a3
C12H2O12 <director20844@gmail.com>
2020-08-14 16:39:54 +0900

    2020.08.14.금 오후 4:37
    프로젝트 View 통신까지 완료(axios get을 통해 값을 받아 포트폴리오 각각의 장소에 데이터를 넣는 역할 추가)
    
    단 Github로 이동하는 v-btn 코드에서 경고가 뜸. 내용은 다음과 같음
    
    [Vue warn]: Invalid prop: type check failed for prop "href". Expected String, Object, got Function
    
    기존의 href가 아닌 :href로 사용하여 오류가 난 것으로 보임
    작동상의 문제는 없으나 추후에 오류 수정 요망

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -6,100 +6,88 @@
             </v-col>
             <v-col cols="12" lg="9">
                 <v-row>
-                    <v-col
-                    cols="12"
-                    class="customTable text-centerZ">
-                        <h2>{{contentsObj.title}}</h2>
+                    <v-col cols="12" class="customTable text-centerZ">
+                        <v-row class="pl-5">
+                            <h2 cols="9">{{this.Project.projectTitle}}</h2>
+
+                            <v-col cols="3" class="py-0">
+                                <v-btn
+                                    :href="this.Project.link"
+                                    target="_blank"
+                                    rounded="rounded"
+                                    depressed="depressed"
+                                    class="customBtn">Github</v-btn>
+                            </v-col>
+                        </v-row>
+
                     </v-col>
                 </v-row>
                 <v-row>
                     <v-col cols="3" sm="2" class="customTable text-center">작성자</v-col>
-                    <v-col cols="9" sm="2" class="text-center">taese0ng</v-col>
+                    <v-col cols="9" sm="2" class="text-center">{{this.Project.writer}}</v-col>
 
                     <v-col cols="3" sm="2" class="customTable text-center">작성일</v-col>
-                    <v-col cols="9" sm="2" class="text-center">2020-10-10</v-col>
+                    <v-col cols="9" sm="2" class="text-center">{{this.Project.date}}</v-col>
 
                     <v-col cols="3" sm="2" class="customTable text-center">view</v-col>
-                    <v-col cols="9" sm="2" class="text-center">00</v-col>
+                    <v-col cols="9" sm="2" class="text-center">{{this.Project.view}}</v-col>
                 </v-row>
 
                 <v-row>
-                    <v-col
-                    cols="3"
-                    sm="2"
-                    class="customTable text-center">
+                    <v-col cols="3" sm="2" class="customTable text-center">
                         팀장
                     </v-col>
-                    <v-col
-                    cols="9"
-                    sm="2"
-                    class="text-center">
-                        김태성
+                    <v-col cols="9" sm="2" class="text-center">
+                        {{this.Project.teamLeaderCode}}
                     </v-col>
 
-                    <v-col
-                    cols="3"
-                    sm="2"
-                    class="customTable text-center">
+                    <v-col cols="3" sm="2" class="customTable text-center">
                         개발기간
                     </v-col>
-                    <v-col
-                    cols="9"
-                    sm="6"
-                    class="text-center">
-                        2020-12-24 ~ 2020-12-25
+                    <v-col cols="9" sm="6" class="text-center">
+                        {{this.Project.date}}
+                        ~
+                        {{this.Project.projectEndDate}}
                     </v-col>
                 </v-row>
 
                 <v-row>
-                    <v-col
-                    cols="12"
-                    md="2"
-                    class="customTable text-center">
+                    <v-col cols="12" md="2" class="customTable text-center">
                         프로젝트 설명
                     </v-col>
 
                     <v-col>
                         <v-card-text class="text-center">
-                            <VueMarkdown>{{contentsObj.contents}}</VueMarkdown>
+                            <VueMarkdown :source="this.Project.contents"></VueMarkdown>
+
                         </v-card-text>
                     </v-col>
                 </v-row>
 
                 <v-row>
-                    <v-col
-                    cols="12"
-                    md="2"
-                    class="customTable text-center">
+                    <v-col cols="12" md="2" class="customTable text-center">
                         팀원
                     </v-col>
-                    <v-col
-                    cols="10"
-                    class="text-center">
+                    <v-col cols="10" class="text-center">
                         <v-row>
-                            <v-col
-                            v-for="member in members"
-                            :key="member.index"
-                            cols="12"
-                            md="6">
+                            <v-col v-for="member in members" :key="member.index" cols="12" md="6">
                                 <v-row align="center">
-                                    <v-col
-                                    cols="6">
+                                    <v-col cols="6">
                                         <v-img
-                                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
-                                        alt="test_img"
-                                        />
+                                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
+                                            alt="test_img"/>
                                     </v-col>
 
-                                    <v-col
-                                    cols="6">
+                                    <v-col cols="6">
                                         {{member}}
                                     </v-col>
                                 </v-row>
                             </v-col>
-                            
+
                         </v-row>
+
                     </v-col>
+
                 </v-row>
             </v-col>
         </v-row>
@@ -111,27 +99,47 @@
     import SubTitle from '../components/SubTitle.vue';
 
     export default {
-        props:{
-            contentsObj:Object
-        },
-        components:{
+        components: {
             VueMarkdown,
-            SubTitle,
+            SubTitle
         },
-        data(){
-            return{
-                members:["15김태성", "16김도현", "16박건웅","18이효진", "18전하영", "20김혜진", "20박수현"],
-                test:'# test',
-                subTitleObj: {title: "프로젝트", contents:"프로젝트이다."}
+        data() {
+            return {
+                Project: "",
+                members: [],
+                test: '# test',
+                subTitleObj: {
+                    title: "프로젝트",
+                    contents: "프로젝트이다."
+                }
             }
+        },
+        methods: {
+            goLinkEvent(){
+                console.log("Why not");
+            }
+        },
+        beforeCreate() {
+            this
+                .axios
+                .get("http://49.50.166.64/api/pf/detail/5f35616dfdd21421cc3a8f76", {})
+                .then((res) => {
+                    // handle success
+                    this.Project = res.data.portfolio[0],
+                    this.members = this.Project.students
+                })
+                .catch((err) => {
+                    // handle error
+                    console.log(err);
+                })
         }
     }
 </script>
 
-<style scoped>
+<style scoped="scoped">
 
-.customTable{
-    background: rgb(56, 82, 103);
-    color: white
-}
+    .customTable {
+        background: rgb(56, 82, 103);
+        color: white;
+    }
 </style>
