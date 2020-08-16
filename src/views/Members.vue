0e20b4e03fd62ec82f1427186d130440b1e3b202
suhyeondev <contact.suhyeon@gmail.com>
2020-08-16 18:08:02 +0900

    2020.8.16. 계속 응답 실패하는 문제를 거의 다 해결해 가는것 같음

--- src/views/Members.vue
+++ src/views/Members.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="members">
     <v-container class="text-center">
-      <h1 class="text-left">{{season}}</h1>
+      <h1 class="text-left">{{activeYear}} {{season}}</h1>
       <v-row>
         <v-col cols="6" sm="3">
           <v-card class="pa-10">
@@ -9,6 +9,7 @@
             <p class="role">{{managementList}}</p>
             <p class="number">{{studentCode}}</p>
             <p class="name">{{name}}</p>
+            <p class="position">{{language}}{{position}}</p>
             <p class="contents">{{contents}}</p>
           </v-card>
         </v-col>
@@ -18,13 +19,14 @@
             <p class="role">{{managementList}}</p>
             <p class="studentCode">{{studentCode}}</p>
             <p class="name">{{name}}</p>
+            <p class="position">{{language}}{{position}}</p>
             <p class="contents">{{contents}}</p>
           </v-card>
         </v-col>
       </v-row>
 
       <v-divider class="mt-16"></v-divider>
-      <h1 class="mt-5 text-left">{{season}}</h1>
+      <h1 class="mt-5 text-left">{{activeYear}} {{season}}</h1>
       <v-row>
         <v-col cols="6" sm="3">
           <v-card class="pa-10">
@@ -32,6 +34,7 @@
             <p class="role">{{managementList}}</p>
             <p class="studentCode">{{studentCode}}</p>
             <p class="name">{{name}}</p>
+            <p class="position">{{language}}{{position}}</p>
             <p class="contents">{{contents}}</p>
           </v-card>
         </v-col>
@@ -41,6 +44,7 @@
             <p class="role">{{managementList}}</p>
             <p class="studentCode">{{studentCode}}</p>
             <p class="name">{{name}}</p>
+            <p class="position">{{language}}{{position}}</p>
             <p class="contents">{{contents}}</p>
           </v-card>
         </v-col>
@@ -50,6 +54,7 @@
             <p class="role">{{managementList}}</p>
             <p class="studentCode">{{studentCode}}</p>
             <p class="name">{{name}}</p>
+            <p class="position">{{language}}{{position}}</p>
             <p class="contents">{{contents}}</p>
           </v-card>
         </v-col>
@@ -59,13 +64,14 @@
             <p class="role">{{managementList}}</p>
             <p class="studentCode">{{studentCode}}</p>
             <p class="name">{{name}}</p>
+            <p class="position">{{language}}{{position}}</p>
             <p class="contents">{{contents}}</p>
           </v-card>
         </v-col>
       </v-row>
 
       <v-divider class="mt-16"></v-divider>
-      <h1 class="mt-5 text-left">{{season}}</h1>
+      <h1 class="mt-5 text-left">{{activeYear}} {{season}}</h1>
       <v-row>
         <v-col cols="6" sm="3">
           <v-card class="pa-10">
@@ -73,6 +79,7 @@
             <p class="role">{{managementList}}</p>
             <p class="studentCode">{{studentCode}}</p>
             <p class="name">{{name}}</p>
+            <p class="position">{{language}}{{position}}</p>
             <p class="contents">{{contents}}</p>
           </v-card>
         </v-col>
@@ -82,6 +89,7 @@
             <p class="role">{{managementList}}</p>
             <p class="studentCode">{{studentCode}}</p>
             <p class="name">{{name}}</p>
+            <p class="position">{{language}}{{position}}</p>
             <p class="contents">{{contents}}</p>
           </v-card>
         </v-col>
@@ -91,6 +99,7 @@
             <p class="role">{{managementList}}</p>
             <p class="studentCode">{{studentCode}}</p>
             <p class="name">{{name}}</p>
+            <p class="position">{{language}}{{position}}</p>
             <p class="contents">{{contents}}</p>
           </v-card>
         </v-col>
@@ -105,37 +114,115 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      season: "2021년 1학기",
-      managementList: "회장",
-      studentCode: "20210000",
-      name: "김이름",
-      contents: `내용내용내용내용내용내용내용내용`,
-      image:
-        "https://t3.ftcdn.net/jpg/01/85/74/32/240_F_185743207_RUJhwfdeIvaEkhZxDyEqbsBZuFVOYRbp.jpg",
+      activeYear: null,
+      season: null,
+      managementList: null,
+      studentCode: null,
+      name: null,
+      contents: null,
+      language: null,
+      position: null,
+      image: null,
     };
   },
 
   mounted() {
-    console.log("111");
-    axios
-      .get("http://49.50.166.64/api/management/list")
+    this.getData();
+  },
+
+  methods: {
+    getData() {
+      axios.get("http://49.50.166.64/api/management/list")
       .then((res) => {
-        console.log("222");
+        console.log("콘솔로그 시작");
         console.log(res);
-        this.season = res.season;
-        this.studentCode = res.studentCode;
-        this.name = res.name;
-        this.image = res.image;
-        this.contents = res.contents;
-        this.managementList = res.managementList;
+        console.log(res.status);
+        console.log(res.management);
+        console.log(res.management.activeYear);
+        console.log(res.management.Info.name);
+        console.log("콘솔로그 끝");
+        this.activeYear = res.management.activeYear;
+        this.season = res.management.season;
+        this.language = res.management.language;
+        this.position = res.management.position;
+        this.studentCode = res.management.studentCode;
+        this.name = res.management.name;
+        this.image = res.management.image;
+        this.contents = res.management.contents;
+        this.managementList = res.management.managementList;
       })
       .catch((error) => {
-        console.log("333");
         console.log(error);
       });
-  },
-};
+    },
+  }
+}
 </script>
+
+<!--
+{"status":"success",
+"management":[
+  {"_id":"5f361dd5573edf62414b6389",
+  "activeYear":"2020",
+  "season":"1학기 (여름학기)",
+  "position":"멘토",
+  "language":"C++",
+  "studentCode":"20160450",
+  "contents":"박건웅이다.",
+  "Info":[{"name":"박건웅","image":"default.jpg"}]},
+  
+  {"_id":"5f362c9d573edf62414b638b",
+  "activeYear":"2016",
+  "season":"2학기 (겨울학기)",
+  "position":"회장",
+  "language":"",
+  "studentCode":"20160001",
+  "contents":"이정철이다",
+  "Info":[{"name":"이정철","image":"default.jpg"}]},
+  
+  {"_id":"5f362e1c573edf62414b638f",
+  "activeYear":"2018",
+  "season":"1학기 (여름학기)",
+  "position":"멘토",
+  "language":"Linux",
+  "studentCode":"20181061",
+  "contents":"전하영이다.",
+  "Info":[{"name":"전하영","image":"default.jpg"}]},
+  
+  {"_id":"5f3634362c290901c807335c",
+  "activeYear":"2020",
+  "season":"2학기 (겨울학기)",
+  "position":"멘토",
+  "language":"Python",
+  "studentCode":"20161313",
+  "contents":"병신새끼이다.",
+  "Info":[{"name":"황윤성","image":"2020-08-16_15-00-56-435_akali.jpg"}]},
+  
+  {"_id":"5f36396d2c290901c807338a",
+  "activeYear":"2016",
+  "season":"1학기 (여름학기)",
+  "position":"멘토","language":"Java",
+  "studentCode":"20160140","contents":"김도현이다.",
+  "Info":[{"name":"김도현","image":"default.jpg"}]},
+  
+  {"_id":"5f36399b2c290901c807338b",
+  "activeYear":"2018",
+  "season":"2학기 (겨울학기)",
+  "position":"멘토","language":"Python",
+  "studentCode":"20180982",
+  "contents":"이효진이다.",
+  "Info":[{"name":"이효진","image":"default.jpg"}]},
+  
+  {"_id":"5f3674cc0fc63e33c6897134",
+  "activeYear":"2012",
+  "season":"2학기 (겨울학기)",
+  "position":"멘토",
+  "language":"C++",
+  "studentCode":"20150000",
+  "contents":"김태성 수정",
+  "Info":[{"name":"김태성","image":"default.jpg"}]}
+]}
+-->
 
 <style>
 img {
