7e7844cb71b6c81abd3c9f9fe6f58b26d4571529
hayoungzzzzz <hayoung474@gmail.com>
2020-08-17 18:21:24 +0900

    간부조회 기능 구현 이미지추가해야함

--- src/views/Management.vue
+++ src/views/Management.vue
@@ -1,119 +1,48 @@
 <template>
   <div id="members">
     <v-container class="text-center">
-      <h1 class="text-left">{{activeYear}} {{season}}</h1>
-      <v-row>
-        <v-col cols="6" sm="3">
-          <v-card class="pa-10">
-            <img v-bind:src="image" />
-            <p class="position">{{language}}{{position}}</p>
-            <p class="number">{{studentCode}}</p>
-            <p class="name">{{name}}</p>
-            <p class="contents">{{contents}}</p>
-          </v-card>
-        </v-col>
-        <v-col cols="6" sm="3">
-          <v-card class="pa-10">
-            <img v-bind:src="image" />
-            <p class="position">{{language}}{{position}}</p>
-            <p class="studentCode">{{studentCode}}</p>
-            <p class="name">{{name}}</p>
-            <p class="contents">{{contents}}</p>
-          </v-card>
-        </v-col>
-      </v-row>
-
-      <v-divider class="mt-16"></v-divider>
-      <h1 class="mt-5 text-left">{{activeYear}} {{season}}</h1>
-      <v-row>
-        <v-col cols="6" sm="3">
-          <v-card class="pa-10">
-            <img v-bind:src="image" />
-            <p class="position">{{language}}{{position}}</p>
-            <p class="studentCode">{{studentCode}}</p>
-            <p class="name">{{name}}</p>
-            <p class="contents">{{contents}}</p>
-          </v-card>
-        </v-col>
-        <v-col cols="6" sm="3">
-          <v-card class="pa-10">
-            <img v-bind:src="image" />
-            <p class="position">{{language}}{{position}}</p>
-            <p class="studentCode">{{studentCode}}</p>
-            <p class="name">{{name}}</p>
-            <p class="contents">{{contents}}</p>
-          </v-card>
-        </v-col>
-        <v-col cols="6" sm="3">
-          <v-card class="pa-10">
-            <img v-bind:src="image" />
-            <p class="position">{{language}}{{position}}</p>
-            <p class="studentCode">{{studentCode}}</p>
-            <p class="name">{{name}}</p>
-            <p class="contents">{{contents}}</p>
-          </v-card>
-        </v-col>
-        <v-col cols="6" sm="3">
-          <v-card class="pa-10">
-            <img v-bind:src="image" />
-            <p class="position">{{language}}{{position}}</p>
-            <p class="studentCode">{{studentCode}}</p>
-            <p class="name">{{name}}</p>
-            <p class="contents">{{contents}}</p>
-          </v-card>
-        </v-col>
-      </v-row>
-
-      <v-divider class="mt-16"></v-divider>
-      <h1 class="mt-5 text-left">{{activeYear}} {{season}}</h1>
-      <v-row>
-        <v-col cols="6" sm="3">
-          <v-card class="pa-10">
-            <img v-bind:src="image" />
-            <p class="position">{{language}}{{position}}</p>
-            <p class="studentCode">{{studentCode}}</p>
-            <p class="name">{{name}}</p>
-            <p class="contents">{{contents}}</p>
-          </v-card>
-        </v-col>
-        <v-col cols="6" sm="3">
-          <v-card class="pa-10">
-            <img v-bind:src="image" />
-            <p class="position">{{language}}{{position}}</p>
-            <p class="studentCode">{{studentCode}}</p>
-            <p class="name">{{name}}</p>
-            <p class="contents">{{contents}}</p>
-          </v-card>
-        </v-col>
-        <v-col cols="6" sm="3">
-          <v-card class="pa-10">
-            <img v-bind:src="image" />
-            <p class="position">{{language}}{{position}}</p>
-            <p class="studentCode">{{studentCode}}</p>
-            <p class="name">{{name}}</p>
-            <p class="contents">{{contents}}</p>
-          </v-card>
-        </v-col>
-        <v-col cols="6" sm="3"></v-col>
-      </v-row>
+      <div v-for="(cadre,i) in CadreList" :key="i">
+        <div v-if="cadre[1][0].length != 0">
+          <h1 class="text-left">{{cadre[0]}}년 1학기 (여름학기)</h1>
+          <v-row>
+            <v-col cols="6" sm="3" v-for="(member,j) in cadre[1][0]" :key="j">
+              <v-card class="pa-10">
+                <img v-bind:src="member.Info[0].image" />
+                <p class="position">{{member.language}}{{member.position}}</p>
+                <p class="number">{{member.studentCode}}</p>
+                <p class="name">{{member.Info[0].name}}</p>
+                <p class="contents">{{member.contents}}</p>
+              </v-card>
+            </v-col>
+          </v-row>
+          <v-divider class="mt-16"></v-divider>
+        </div>
+        <div v-if="cadre[1][1].length != 0">
+          <h1 class="text-left">{{cadre[0]}}년 2학기 (겨울학기)</h1>
+          <v-row>
+            <v-col cols="6" sm="3" v-for="(member,j) in cadre[1][1]" :key="j">
+              <v-card class="pa-10">
+                <img v-bind:src="member.Info[0].image" />
+                <p class="position">{{member.language}}{{member.position}}</p>
+                <p class="number">{{member.studentCode}}</p>
+                <p class="name">{{member.Info[0].name}}</p>
+                <p class="contents">{{member.contents}}</p>
+              </v-card>
+            </v-col>
+          </v-row>
+          <v-divider class="mt-16"></v-divider>
+        </div>
+      </div>
     </v-container>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
   data() {
     return {
-      activeYear: null,
-      season: null,
-      managementList: null,
-      studentCode: null,
-      name: null,
-      contents: null,
-      language: null,
-      position: null,
-      image: null,
+      CadreList: [],
     };
   },
 
@@ -123,101 +52,48 @@ export default {
 
   methods: {
     getData() {
-      axios.get("http://49.50.166.64/api/management/list")
-      .then((res) => {
-        console.log("콘솔로그 시작");
-        // console.log(res);
-        // console.log(res.status);
-        // console.log(res.data);
-        // console.log(res.data.management);
-        // console.log(res.data.management[0]);
-        console.log(res.data.management[0].Info);
-        // console.log(res.data.management[0].activeYear);
-        // console.log(res.data.management[0].image);
-        console.log("콘솔로그 끝");
-
-        this.activeYear = `${res.data.management[0].activeYear}년`;
-        this.season = res.data.management[0].season;
-        this.language = res.data.management[0].language;
-        this.position = res.data.management[0].position;
-        this.studentCode = res.data.management[0].studentCode;
-        this.name = res.data.management[0].Info[0].name;
-        this.image = `http://49.50.166.64/${res.data.management[0].Info[0].image}`;
-        this.contents = res.data.management[0].contents;
-        this.managementList = res.data.management[0].managementList;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+      axios
+        .get("http://49.50.166.64/api/management/list")
+        .then((res) => {
+          this.CadreList = [];
+          var managementList = res.data.management;
+          console.log(managementList);
+          managementList.forEach((element) => {
+            var flag = false;
+            if (this.CadreList.length === 0) {
+              if (element.season === "2학기 (겨울학기)") {
+                this.CadreList.push([element.activeYear, [[], [element]]]);
+              } else if (element.season === "1학기 (여름학기)") {
+                this.CadreList.push([element.activeYear, [[element], []]]);
+              }
+            } else {
+              for (let i = 0; i < this.CadreList.length; i++) {
+                if (this.CadreList[i][0] === element.activeYear) {
+                  if (element.season === "1학기 (여름학기)") {
+                    this.CadreList[i][1][0].push(element);
+                  } else if (element.season === "2학기 (겨울학기)") {
+                    this.CadreList[i][1][1].push(element);
+                  }
+                  flag = true;
+                }
+              }
+              if (flag === false) {
+                if (element.season === "2학기 (겨울학기)") {
+                  this.CadreList.push([element.activeYear, [[], [element]]]);
+                } else if (element.season === "1학기 (여름학기)") {
+                  this.CadreList.push([element.activeYear, [[element], []]]);
+                }
+              }
+            }
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-  }
-}
+  },
+};
 </script>
-
-<!--
-{"status":"success",
-"management":[
-  {"_id":"5f361dd5573edf62414b6389",
-  "activeYear":"2020",
-  "season":"1학기 (여름학기)",
-  "position":"멘토",
-  "language":"C++",
-  "studentCode":"20160450",
-  "contents":"박건웅이다.",
-  "Info":[{"name":"박건웅","image":"default.jpg"}]},
-  
-  {"_id":"5f362c9d573edf62414b638b",
-  "activeYear":"2016",
-  "season":"2학기 (겨울학기)",
-  "position":"회장",
-  "language":"",
-  "studentCode":"20160001",
-  "contents":"이정철이다",
-  "Info":[{"name":"이정철","image":"default.jpg"}]},
-  
-  {"_id":"5f362e1c573edf62414b638f",
-  "activeYear":"2018",
-  "season":"1학기 (여름학기)",
-  "position":"멘토",
-  "language":"Linux",
-  "studentCode":"20181061",
-  "contents":"전하영이다.",
-  "Info":[{"name":"전하영","image":"default.jpg"}]},
-  
-  {"_id":"5f3634362c290901c807335c",
-  "activeYear":"2020",
-  "season":"2학기 (겨울학기)",
-  "position":"멘토",
-  "language":"Python",
-  "studentCode":"20161313",
-  "contents":"병신새끼이다.",
-  "Info":[{"name":"황윤성","image":"2020-08-16_15-00-56-435_akali.jpg"}]},
-  
-  {"_id":"5f36396d2c290901c807338a",
-  "activeYear":"2016",
-  "season":"1학기 (여름학기)",
-  "position":"멘토","language":"Java",
-  "studentCode":"20160140","contents":"김도현이다.",
-  "Info":[{"name":"김도현","image":"default.jpg"}]},
-  
-  {"_id":"5f36399b2c290901c807338b",
-  "activeYear":"2018",
-  "season":"2학기 (겨울학기)",
-  "position":"멘토","language":"Python",
-  "studentCode":"20180982",
-  "contents":"이효진이다.",
-  "Info":[{"name":"이효진","image":"default.jpg"}]},
-  
-  {"_id":"5f3674cc0fc63e33c6897134",
-  "activeYear":"2012",
-  "season":"2학기 (겨울학기)",
-  "position":"멘토",
-  "language":"C++",
-  "studentCode":"20150000",
-  "contents":"김태성 수정",
-  "Info":[{"name":"김태성","image":"default.jpg"}]}
-]}
--->
 
 <style>
 v-card img {
