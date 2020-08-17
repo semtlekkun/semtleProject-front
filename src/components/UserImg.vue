273c7cc6c19838107bcc0020842aa452d4d7b4d6
gunwoongPark <bung1438@gmail.com>
2020-08-17 20:04:16 +0900

    이미지 통신 끝

--- src/components/UserImg.vue
+++ src/components/UserImg.vue
@@ -5,40 +5,31 @@
         <!-- 회원정보 변경 화면에서 보여질 카드 -->
         <v-card height="200" light="light">
           <div class="text-center">
-            <img height="150" :src="imageUrl" />
+            <img height="145" :src="imageUrl" />
           </div>
 
-          <v-card-actions style="justify-content:center;">
-            <v-btn v-bind="attrs" v-on="on" dark="dark" class="btn">이미지 변경</v-btn>
+          <v-card-actions class="justify-center">
+            <v-btn v-bind="attrs" v-on="on" dark class="btn" @click="openDialog">이미지 변경</v-btn>
           </v-card-actions>
         </v-card>
       </template>
 
-      <div class="white lighten-1">
+      <v-card class="white lighten-1">
         <v-card-title class="headline lighten-2">이미지 변경</v-card-title>
 
         <v-divider></v-divider>
-
-        <!-- 
-                <v-alert type="error" v-model="emptyfile">
-                    이미지 파일이 존재하지 않습니다.
-                </v-alert>
-        -->
-
         <!-- 이미지 변경 다이얼로그 창 내용구성 -->
         <v-container>
-          <v-container>
-            <v-row>
-              <v-col>
-                <v-file-input
-                  label="이미지 첨부"
-                  accept="image/*"
-                  prepend-icon="mdi-camera"
-                  v-model="imageUrl"
-                ></v-file-input>
-              </v-col>
-            </v-row>
-          </v-container>
+          <v-row>
+            <v-col>
+              <v-file-input
+                label="이미지 첨부"
+                accept="image/*"
+                prepend-icon="mdi-camera"
+                v-model="selectImg"
+              ></v-file-input>
+            </v-col>
+          </v-row>
         </v-container>
 
         <v-divider></v-divider>
@@ -47,7 +38,7 @@
           <v-spacer></v-spacer>
           <v-btn class="btn btn-dark" text="text" @click="submit">확인</v-btn>
         </v-card-actions>
-      </div>
+      </v-card>
     </v-dialog>
   </div>
 </template>
@@ -57,33 +48,47 @@ export default {
   data: () => ({
     dialog: false,
     img: "../assets/sample.png",
-    selectImg: null,
     data: null,
     imageUrl: null,
+    selectImg: null,
   }),
   created() {
     this.initImage();
   },
   methods: {
-    submit() {
-      console.log(this.imageUrl);
-      //
-
-      this.dialog = false;
-    },
     initImage() {
       let config = {
         headers: { token: sessionStorage.getItem("token") },
       };
-
-      console.log(config);
 
       this.axios
         .get("http://49.50.166.64/api/mypage", config)
         .then((res) => {
           this.imageUrl = "http://49.50.166.64/api/student/";
           this.imageUrl = this.imageUrl.concat(res.data.student.image);
-          console.log(this.imageUrl);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    openDialog() {
+      this.selectImg = null;
+    },
+    submit() {
+      let config = {
+        headers: { token: sessionStorage.getItem("token") },
+      };
+
+      let form = new FormData();
+      form.append("image", this.selectImg);
+
+      this.axios
+        .put("http://49.50.166.64/api/mypage/picture/update", form, config)
+        .then((res) => {
+          alert("이미지 변경 성공!");
+          console.log(res);
+          this.dialog = false;
+          this.$router.go();
         })
         .catch((err) => {
           console.log(err);
