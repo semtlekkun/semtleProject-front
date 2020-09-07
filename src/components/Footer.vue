b1da04dd02a7b885ad186e6f644b69160c206433
gunwoongPark <bung1438@gmail.com>
2020-09-08 02:21:02 +0900

    footer 가 콘텐츠를 잡아먹는 경우 해결

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,37 +1,35 @@
 <template>
-    <v-footer absolute>
-        <v-col cols="12">
-            <v-row class="justify-center">
-                <v-col cols="12" sm="6" md="4" lg="4" xl="3">
-                    <h4>문의 : 셈틀꾼 (부)회장</h4>
-                </v-col>
-                <v-col cols="12" sm="6" md="4" lg="4" xl="3">
-                    <h4>찾아오는 길 : 컴퓨터공학과 셈틀꾼</h4>
-                </v-col>
-                <v-col cols="12" sm="6" md="4" lg="4" xl="3">
-                    <h4>소속 : 금오공과대학교 컴퓨터공학과</h4>
-                </v-col>
-            </v-row>
+  <v-footer absolute style="height:25px;">
+    <v-col cols="12">
+      <v-row class="justify-center">
+        <v-col cols="12" sm="6" md="4" lg="4" xl="3">
+          <h4>문의 : 셈틀꾼 (부)회장</h4>
         </v-col>
+        <v-col cols="12" sm="6" md="4" lg="4" xl="3">
+          <h4>찾아오는 길 : 컴퓨터공학과 셈틀꾼</h4>
+        </v-col>
+        <v-col cols="12" sm="6" md="4" lg="4" xl="3">
+          <h4>소속 : 금오공과대학교 컴퓨터공학과</h4>
+        </v-col>
+      </v-row>
+    </v-col>
 
-        <v-col class="text-center" cols="12">
-            <h4 id="footerCopyright">&copy; Copyright {{ new Date().getFullYear() }} 셈틀꾼</h4>
-        </v-col>
-    </v-footer>
+    <v-col class="text-center" cols="12">
+      <h4 id="footerCopyright">&copy; Copyright {{ new Date().getFullYear() }} 셈틀꾼</h4>
+    </v-col>
+  </v-footer>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style scoped>
-    #footerCopyright{
-        color: gray;
-    }
+#footerCopyright {
+  color: gray;
+}
 
-    h4{
-        color:#659ab5;
-    }
+h4 {
+  color: #659ab5;
+}
 </style>
