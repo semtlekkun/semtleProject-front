954375bfa21bd3824ff9301dce9608f47e9e34fd
gunwoongPark <olympiodoros@naver.com>
2021-08-01 20:51:16 +0900

    dummy 이미지 적용

--- src/components/PhotoContainer.vue
+++ src/components/PhotoContainer.vue
@@ -1,12 +1,43 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="12" md="3" sm="4"> </v-col>
-    </v-row>
-  </v-container>
+  <v-row class="photoContainer">
+    <v-col
+      cols="4"
+      lg="2"
+      md="2"
+      sm="4"
+      xs="4"
+      v-for="(photo, idx) in photoList"
+      :key="idx"
+    >
+      <v-img :src="photo"></v-img>
+    </v-col>
+  </v-row>
 </template>
 <script>
-export default {};
+import DummyImage from "../assets/쿼카.jpg";
+
+export default {
+  data() {
+    return {
+      photoList: [
+        DummyImage,
+        DummyImage,
+        DummyImage,
+        DummyImage,
+        DummyImage,
+        DummyImage,
+      ],
+    };
+  },
+};
 </script>
 <style scoped="scoped">
+.photoContainer {
+  text-align: -webkit-center;
+}
+.testBox {
+  width: 100px;
+  height: 100px;
+  background: tomato;
+}
 </style>
