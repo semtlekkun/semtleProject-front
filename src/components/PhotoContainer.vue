8bb2c9466c3cf2b699e2a5cd14dd9c0060adaecc
gunwoongPark <olympiodoros@naver.com>
2021-08-02 14:40:36 +0900

    태깅 및 자료구조 변경

--- src/components/PhotoContainer.vue
+++ src/components/PhotoContainer.vue
@@ -9,7 +9,9 @@
       v-for="(photo, idx) in photoList"
       :key="idx"
     >
-      <v-img :src="photo"></v-img>
+      <router-link :to="photo.url">
+        <v-img :src="photo.thumbNail"></v-img>
+      </router-link>
     </v-col>
   </v-row>
 </template>
@@ -19,15 +21,39 @@ import DummyImage from "../assets/쿼카.jpg";
 export default {
   data() {
     return {
+      // photoList는 최초의 빈 배열로 생성
       photoList: [
-        DummyImage,
-        DummyImage,
-        DummyImage,
-        DummyImage,
-        DummyImage,
-        DummyImage,
+        {
+          thumbNail: DummyImage,
+          url: "어쩌고저쩌고",
+        },
+        {
+          thumbNail: DummyImage,
+          url: "어쩌고저쩌고",
+        },
+        {
+          thumbNail: DummyImage,
+          url: "어쩌고저쩌고",
+        },
+        {
+          thumbNail: DummyImage,
+          url: "어쩌고저쩌고",
+        },
+        {
+          thumbNail: DummyImage,
+          url: "어쩌고저쩌고",
+        },
+        {
+          thumbNail: DummyImage,
+          url: "어쩌고저쩌고",
+        },
       ],
     };
+  },
+
+  created() {
+    // axios 비동기 통신으로 사진을 최대 6개 얻어와서
+    // photoList에 할당
   },
 };
 </script>
