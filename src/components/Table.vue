2a830586acb34942bdff45fc18e9bb48aec26ed2
hj-language <idgafff@kumoh.ac.kr>
2021-08-06 16:34:53 +0900

    Table sort기능 제거

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -67,17 +67,20 @@ export default {
         {
           text: "제목",
           align: "start",
+          sortable: false,
           value: "title",
         },
         {
           text: "작성자",
           align: "center",
+          sortable: false,
           value: "writer",
           width: "120px",
         },
         {
           text: "작성 일시",
           align: "center",
+          sortable: false,
           value: "date",
           width: "140px",
         },
